14fb8ac6a1d0dc85856beade298d33cdeecc08e4
kelidas <kelidas@centrum.cz>
2012-03-01 10:46:53 +0100

    test_2 added

---
+++ first/test_2.py
@@ -0,0 +1,7 @@
+'''
+Created on Mar 1, 2012
+
+@author: kelidas
+'''
+def test():
+    print 'test 2'
--- second/test_1.py
+++ second/test_1.py
@@ -3,3 +3,7 @@ Created on Feb 29, 2012
 
 @author: kelidas
 '''
+print 'test'
+
+from test_2 import test
+test()
