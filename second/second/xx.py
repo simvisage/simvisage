756ecb542f371e9e19c80c2a5a84304e7d9f41e1
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2012-03-02 17:17:21 +0100

    dependencies seem to work

---
+++ second/second/xx.py
@@ -0,0 +1,12 @@
+'''
+Created on Mar 2, 2012
+
+@author: rch
+'''
+
+import first
+
+import ibvpy
+
+import util
+
---
+++ stats/test.py
@@ -0,0 +1,13 @@
+'''
+Created on Mar 2, 2012
+
+@author: rch
+'''
+
+import sys
+
+print sys.path
+
+import stats
+
+import util
