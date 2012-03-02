72a57c8d5703bf4c729244909722ef42ca19ada2
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2012-03-02 08:19:24 +0100

    stats imported into the github

---
+++ stats/stats/random_field/gauss_1D.py
@@ -0,0 +1,82 @@
+'''
+Created on 24.06.2011
+
+@author: rrypl
+'''
+
+from enthought.traits.api import HasTraits, Float, Array, Int, Property, \
+    cached_property, Bool
+from math import e
+from matplotlib import pyplot as p
+from numpy import dot, transpose, ones, array, eye, real, linspace, reshape
+from numpy.linalg import eig
+from numpy.random import shuffle
+from scipy.linalg import toeplitz
+from scipy.stats import norm
+
+
+def acor( dx, lcorr ):
+    '''autocorrelation function'''
+    return e ** ( -( dx / lcorr ) ** 2 )
+
+class GaussRandomField( HasTraits ):
+    '''Generating Gaussian distributed random field by scaling a standardized normal distribution random
+     field. The random field array is stored in the property random_field'''
+
+    #Parameters to be set
+    lacor = Float( 1. , auto_set = False, enter_set = True,
+                   desc = 'autocorrelation  of the field', modified = True )
+    nsim = Int( 1 , auto_set = False, enter_set = True,
+                desc = 'No of Fields to be simulated', modified = True )
+    mean = Float( 0, auto_set = False, enter_set = True,
+                  desc = 'mean value', modified = True )
+    stdev = Float( 1., auto_set = False, enter_set = True,
+                   desc = 'standard deviation', modified = True )
+
+    xgrid = Array
+
+    reevaluate = Bool( False )
+
+    eigenvalues = Property( depends_on = '+modified' )
+    @cached_property
+    def _get_eigenvalues( self ):
+        '''evaluates the eigenvalues and eigenvectors of the autocorrelation matrix'''
+        #creating a symm. toeplitz matrix with (xgrid, xgrid) data points
+        Rdist = toeplitz( self.xgrid, self.xgrid )
+        #apply the autocorrelation func. to get the correlation matrix
+        R = acor( Rdist , self.lacor )
+        #evaluate the eigenvalues and eigenvectors of the autocorrelation matrix
+        print 'evaluating eigenvalues for random field...'
+        eigenvalues = eig( R )
+        print 'complete'
+        return eigenvalues
+
+    random_field = Property( Array , depends_on = '+modified, reevaluate' )
+    @cached_property
+    def _get_random_field( self ):
+        '''simulates the Gaussian random field'''
+        #evaluate the eigenvalues and eigenvectors of the autocorrelation matrix
+        _lambda, phi = self.eigenvalues
+        #simulation points from 0 to 1 with an equidistant step for the LHS
+        randsim = linspace( 0, 1, len( self.xgrid ) + 1 ) - 0.5 / ( len( self.xgrid ) )
+        randsim = randsim[1:]
+        #shuffling points for the simulation
+        shuffle( randsim )
+        #matrix containing standard Gauss distributed random numbers
+        xi = transpose( ones( ( self.nsim, len( self.xgrid ) ) ) * array( [ norm().ppf( randsim ) ] ) )
+        #eigenvalue matrix 
+        LAMBDA = eye( len( self.xgrid ) ) * _lambda
+        #cutting out the real part
+        ydata = dot( dot( phi, ( LAMBDA ) ** 0.5 ), xi ).real
+        # scaling the std. distribution
+        scaled_ydata = ydata * self.stdev + self.mean
+        self.reevaluate = False
+        return reshape( scaled_ydata, len( self.xgrid ) )
+
+
+
+if __name__ == '__main__':
+    rf = GaussRandomField( lacor = 6. , xgrid = linspace( 0, 100., 100 ) , nsim = 1 , stdev = 1. )
+    x = rf.xgrid
+    p.plot( x, rf.random_field )
+    p.show()
