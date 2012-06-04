4f66fc87388696a0ecc61bc48e1677454ad60b51
ale-x <ascholzen@imb.rwth-aachen.de>
2012-06-04 16:46:54 +0200

    aramis file with points3d and transformation

--- quaducom/quaducom/assess/assess_shell/get_sig_fl_iterative_rch.py
+++ quaducom/quaducom/assess/assess_shell/get_sig_fl_iterative_rch.py
@@ -4,19 +4,19 @@ Created on Jun 23, 2010
 @author: alexander
 '''
 
-from enthought.traits.api import \
+from etsproxy.traits.api import \
     Float, Instance, Array, Int, Property, cached_property, on_trait_change, Bool, \
     HasTraits, File, Event, Trait
 
-from enthought.traits.ui.api import \
+from etsproxy.traits.ui.api import \
     View, Item, FileEditor, HSplit, Group, VSplit, \
     Handler
 
-from enthought.traits.ui.menu import \
+from etsproxy.traits.ui.menu import \
     Action, CloseAction, HelpAction, Menu, \
     MenuBar, NoButtons, Separator, ToolBar
 
-from enthought.pyface.api import ImageResource
+from etsproxy.pyface.api import ImageResource
 
 from ibvpy.mats.mats_explore import MATSExplore
 from ibvpy.mats.mats2D.mats2D_explore import MATS2DExplore
--- quaducom/quaducom/devproc/exp_browse.py
+++ quaducom/quaducom/devproc/exp_browse.py
@@ -13,10 +13,19 @@ simdb = SimDB()
 
 from matresdev.db.exdb.ex_run_view import ExRunView
 
-ex_path = os.path.join(simdb.exdata_dir, 'tensile_tests', 'dog_bone', '2010-12-07_TT-3g-1cm-a-FR_ibac', 'with_short_fibres', 'TT-3g-2cm-0-90-0-FR-wSF-ibac-V1.DAT')
+#ex_path = os.path.join(simdb.exdata_dir, 'tensile_tests', 'dog_bone', '2010-12-07_TT-3g-1cm-a-FR_ibac', 'with_short_fibres', 'TT-3g-2cm-0-90-0-FR-wSF-ibac-V1.DAT')
+#ex_path = os.path.join(simdb.exdata_dir, 'tensile_tests', 'dog_bone', '2012-04-12_TT-12c-6cm-0-TU_SH4', 'TT-12c-6cm-0-TU-SH4-V1.DAT')
+ex_path = os.path.join(simdb.exdata_dir, 'tensile_tests', 'dog_bone', '2012-02-14_TT-12c-6cm-0-TU_SH2', 'TT-12c-6cm-0-TU-SH2-V1.DAT')
 #ex_path = os.path.join(simdb.exdata_dir, 'bending_tests', 'four_point', '2012-04-03_BT-4PT-12c-6cm-0-TU',
 #                                         'BT-4PT-12c-6cm-SH4', 'BT-4PT-12c-6cm-SH4-V1.DAT')
 
 
 doe_reader = ExRunView(data_file = ex_path)
+
+#print 'model', doe_reader.model.ex_type.Kraft
+#print 'model', doe_reader.model.ex_type.WA_VL 
+#print 'model', doe_reader.model.ex_type.WA_VR
+#print 'model', doe_reader.model.ex_type.WA_HL
+#print 'model', doe_reader.model.ex_type.WA_HR
+
 doe_reader.configure_traits()
