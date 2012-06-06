c91a87501213eb4dfc8d345439e072522e77294d
rostar <rrypl@.imb.rwth-aachen.de>
2012-06-06 16:53:41 +0200

    ACK model structure prepared

--- quaducom/quaducom/meso/ctt/scm_analytical/ack_model.py
+++ quaducom/quaducom/meso/ctt/scm_analytical/ack_model.py
@@ -3,3 +3,14 @@ Created on Apr 20, 2012
 
 @author: rostar
 '''
+
+from material import Material
+from etsproxy.traits.api import Array, Property
+
+
+class ACK(Material):
+    
+    sigma = Array
+    eps = Property()
+    def _get_eps(self):
+        return 1.0
