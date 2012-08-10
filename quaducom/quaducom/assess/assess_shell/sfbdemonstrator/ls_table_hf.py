8d98cb20b8d3996509728bc18e00e55b6ab3dff4
ale-x <ascholzen@imb.rwth-aachen.de>
2012-08-10 18:12:47 +0200

    minor changes

--- quaducom/quaducom/assess/assess_shell/sfbdemonstrator/ls_table_hf.py
+++ quaducom/quaducom/assess/assess_shell/sfbdemonstrator/ls_table_hf.py
@@ -48,9 +48,6 @@ from math import pi
 from string import split
 import os
 
-from ls_table import \
-    LSArrayAdapter
-
 from matresdev.db.simdb import \
     SimDB
     
@@ -63,6 +60,20 @@ from os.path import join
 #
 simdb = SimDB()
 
+
+class LSArrayAdapter (TabularAdapter):
+
+    columns = Property
+    def _get_columns(self):
+#        print 'GETTING COLUMNS', self.object.columns, self.object, self.object.__class__
+        columns = self.object.columns
+        return [ (name, idx) for idx, name in enumerate(columns) ]
+
+    font = 'Courier 10'
+    alignment = 'right'
+    format = '%5.2f'#'%g'
+    even_bg_color = Color(0xE0E0FF)
+    width = Float(80)
 
 
 class LS( HasTraits ):
