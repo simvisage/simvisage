f92040445ef18c996f2f6dbc913ebdad2410a8a6
ale-x <ascholzen@imb.rwth-aachen.de>
2012-08-22 11:50:32 +0200

    construct nu-mu-diagram using 'sig_fl_calib'; comments added; still todo: configure plot style

--- quaducom/quaducom/assess/assess_shell/construct_failure_envelope_nm.py
+++ quaducom/quaducom/assess/assess_shell/construct_failure_envelope_nm.py
@@ -55,31 +55,33 @@ if __name__ == '__main__':
                                width = 0.20,
                                n_roving = 23.,
 
-#                               # values per m
-#                               #
-#                               width = 1.0,
-#                               n_roving = 120,
-
                                # define shape of the crack-bridge law ('linear', 'bilinear' or 'quadratic')
                                #
-                               calc_mode = 'calib',
-#                               calib_config = 'linear',
+#                               calib_config = 'root2',
 #                               calib_config = 'quadratic',
-                               calib_config = 'cubic',
+#                               calib_config = 'linear',  
+#                               calib_config = 'quadratic_eps_max', 
+#                               calib_config = 'quadratic_monoton', 
+#                               calib_config = 'quadratic_TT',  
+#                               calib_config = 'plastic', 
+#                               calib_config = 'cubic',
 
                                # define shape of the concrete stress-strain-law ('block', 'bilinear' or 'quadratic')
                                #
-                               sig_c_config = 'quadratic'
+                              sig_c_config = 'quadratic'
 #                               sig_c_config = 'bilinear'
-                              
+#                               sig_c_config = 'block'
                               )
 
+    
     print '1 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
     sig_fl_calib.calib_sig_t_mfn()
+    u_sol = sig_fl_calib.u_sol
+    max_sig = sig_fl_calib.get_sig_max( u_sol )     
     print 'eps_c_fail', sig_fl_calib.eps_c_fail
     print 'eps_t_fail', sig_fl_calib.eps_t_fail
-    u_sol = sig_fl_calib.u_sol
+    print 'max_sig', sig_fl_calib.get_sig_max( u_sol )                      
 #    sig_fl_calib.plot_sig_t_mfn( u_sol )
     
     
@@ -90,27 +92,13 @@ if __name__ == '__main__':
 
     print '2 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
     
-    eps_lo = 0.005
-    eps_up = -0.002
-    
+    # reproduce the forces for the calibration test:
+    #
+    eps_lo =   sig_fl_calib.eps_t_fail
+    eps_up = - sig_fl_calib.eps_c_fail
     N_internal, M_internal = sig_fl_calib.eval_N_M( eps_lo, eps_up )
 
-    # select the right stress_mode for calculation
-    #
-    u_sol = [ abs(sig_fl_calib.eps_lo), abs(sig_fl_calib.eps_up) ]
-    max_sig = sig_fl_calib.get_sig_max( u_sol )     
-    print 'eps_up', abs( sig_fl_calib.eps_up )
-    print 'eps_lo', abs( sig_fl_calib.eps_lo )
-    print 'eps_c_fail', sig_fl_calib.eps_c_fail
-    print 'eps_t_fail', sig_fl_calib.eps_t_fail
-    print 'max_sig', sig_fl_calib.get_sig_max( u_sol )                      
     
-    # plot response:
-    # graph shows sig_comp at the height of the textil layer in [MPa] 
-    #
-#    sig_fl_calib.plot_sig_t_mfn( u_sol )
-
-
     #------------------------------------------------
     # 3 construct failure envelope for a given value of eps_n 
     #------------------------------------------------
@@ -120,9 +108,10 @@ if __name__ == '__main__':
     n_B = 20
     n_C = 20
 
-    eps_c_fail = sig_fl_calib.eps_c_fail
+    eps_c_fail = 3.3/1000.
     eps_t_fail = sig_fl_calib.eps_t_fail
-
+    print'eps_t_fail ',eps_t_fail
+    
     eps_lo_arr_A = np.linspace( eps_t_fail,   eps_t_fail, n_A )
     eps_up_arr_A = np.linspace( eps_t_fail, - eps_c_fail, n_A )
     
@@ -134,13 +123,13 @@ if __name__ == '__main__':
         
     eps_lo_arr = np.hstack([ eps_lo_arr_A, eps_lo_arr_B, eps_lo_arr_C ])
     eps_up_arr = np.hstack([ eps_up_arr_A, eps_up_arr_B, eps_up_arr_C ])
-
-
-#    various diagrams with a specific value for n_layers and thickness are plotted
+    
 # ------------------------------------------------------------------------------------
 
     import pylab as p
     from mpl_toolkits.axes_grid.axislines import SubplotZero
+    from matplotlib.ticker import MaxNLocator
+    from matplotlib.ticker import AutoMinorLocator
 
     fig = p.figure(1)
     fig.set_facecolor('w')
@@ -155,7 +144,6 @@ if __name__ == '__main__':
     for direction in ["left", "right", "bottom", "top"]:
         ax.axis[direction].set_visible(False)
 
-    
     nu_list = []
     mu_list = []
     N_list = []
@@ -167,24 +155,31 @@ if __name__ == '__main__':
     A_roving = sig_fl_calib.A_roving
     n_rovings = sig_fl_calib.n_rovings
 
+
     # constant 'n_layers':
     #
 #    n_layers_list  = [  12,   12,    12,   12,   12]
 #    thickness_list = [0.02, 0.04,  0.06, 0.08, 0.10]
-    
+#    zip_list = zip( n_layers_list, thickness_list  )
+#    legend_list = thickness_list 
+
+
     # constant 'thickness':
     #
     n_layers_list  = [   4,    8,    12,   16,   20]
     thickness_list = [0.06, 0.06,  0.06, 0.06, 0.06]
-
+    zip_list = zip( n_layers_list, thickness_list  )
+    legend_list = n_layers_list 
+    
     # constant 'rho_tex':
     #
 #    n_layers_list  = [   6,    8,    12,   18,   24]
 #    thickness_list = [0.03, 0.04,  0.06, 0.09, 0.12]
-
-    zip_list = zip( n_layers_list, thickness_list  )
+#    zip_list = zip( n_layers_list, thickness_list  )
+#    legend_list = n_layers_list 
 
     # varying layers for two different heights':
+    # use plot with M,N instead of nu,mu !
     #
 #    n_layers_list = np.array([8,12,18])
 #    thickness_list = np.array ([0.04,0.06])
@@ -202,6 +197,11 @@ if __name__ == '__main__':
         eval_N_M_vectorized = np.frompyfunc( sig_fl_calib.eval_N_M, 2, 2 )
         N_arr, M_arr = eval_N_M_vectorized( eps_lo_arr, eps_up_arr )
         #
+        # @todo: check which value to be used for normalization if sig_c = parabolic is used
+        # NOTE that for this case f_cm  is used not f_ck; NOTE that for pure compression eps = 2E-3 is used
+        # which does not yield f_cm in the parabolic law; for the default concrete laws for pure
+        # compression the value should evaluated to 1.0!
+        #
         c1 = 1000. * t* b * f_ck
         c2 = 1000. * t**2. * b * f_ck 
         #
@@ -218,50 +218,75 @@ if __name__ == '__main__':
         
         # plot normalized values nu and mu
         #
-#        if t == 0.06 and n == 12:
-#            p.plot( mu_arr, nu_arr,
-#                    color='blue', 
-#                    linewidth=2.0)
-#        else:
-#            p.plot( mu_arr, nu_arr )
-
-        # plot absolute values for N and M
-        #
         if t == 0.06 and n == 12:
-            p.plot( M_arr, N_arr,
+            p.plot( mu_arr, nu_arr,
                     color='blue', 
                     linewidth=2.0)
         else:
-            p.plot( M_arr, N_arr )
+            p.plot( mu_arr, nu_arr )
 
- 
-    # @todo: format plot and axes (legend, grid, font etc.)
-#    p.legend( rho_list )
-#    p.grid()
+
+        # plot absolute values for N and M
+        #
+#        if t == 0.06 and n == 12:
+#            p.plot( M_arr, N_arr,
+#                    color='blue', 
+#                    linewidth=2.0)
+#        else:
+#            p.plot( M_arr, N_arr )
+
+    
+    
+    
+    ######### @todo: format plot and axes (legend, grid, font etc.) #########
+
+    ### GRID
+    #
+#    p.grid(b=None, which='both')
+
+
+    ### LEGEND
+    #
+    p.legend( legend_list )
 #    legend( (l2, l4), ('oscillatory', 'damped'), 'upper right', shadow=True)
+
+
+    ### TICKS
+    #
 #    p.axis([0, 6, 250 ,-700])
-#    p.xticks(0.5*np.arange(12))
+    #p.xticks(0.5*np.arange(12))
 #    ax.set_xticks([0., 1., 2., 3., 4., 5., 6.])
     ax.set_ylim(ax.get_ylim()[::-1])
-
-    from matplotlib.ticker import MaxNLocator
-#    ax.xaxis.set_major_locator(MaxNLocator(10))
-    ax.yaxis.set_major_locator(MaxNLocator(10))
-        
-#    p.tick_params(axis='x',direction='out', length=6, width=2, colors='r')
     
-    p.title('Interaction Diagram M/N')
+    ax.xaxis.set_major_locator(MaxNLocator(10))
+    ax.yaxis.set_major_locator(MaxNLocator(10))
+    minorLocator   = AutoMinorLocator()
+    ax.xaxis.set_minor_locator(minorLocator)
+#    p.tick_params(axis='both',which='major',direction='out', length=15, width=2, colors='black')
+#    p.tick_params(axis='both',which='minor',direction='in', length=100, width=1, colors='black')
+
+
+    ### TITEL
+    #
+#    p.title(r'$\nu$ -$\mu$ Interaction Diagram ',fontsize=22,
+#                                verticalalignment='baseline',
+#                                horizontalalignment = 'center')
+    #p.title('Interaction Diagram M/N')
 #    p.xlabel('moment [kNm]')
 #    p.ylabel('normal force [kN]')
 
-    p.xlabel('mu')
-    p.ylabel('nu')
-
+    
+    ### LABELS
+    #
+    #p.xlabel(r'$\mu$',fontsize='20',
+#             verticalalignment = 'top',
+#             horizontalalignment = 'left')
+    #p.ylabel(r'$\nu$ ', fontsize= 20)
 #    from matplotlib import rcParams
 #    rcParams['text.usetex']=True
 #    rcParams['text.latex.unicode']=True
 #    p.xlabel(r"\textbf{$\mu$}")
-#    p.ylabel(r"\textbf{$\nu$}")
+ #  p.ylabel(r\textbf{$\nu$}")
     
     p.show()
     
