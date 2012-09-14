73999d769a4b89bda05658f9aa1e1c169c563994
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2012-09-14 14:23:08 +0200

    plotting with dashed line works now

--- quaducom/quaducom/assess/assess_shell/construct_failure_envelope_nm.py
+++ quaducom/quaducom/assess/assess_shell/construct_failure_envelope_nm.py
@@ -114,11 +114,11 @@ if __name__ == '__main__':
 #        ax.axis[direction].set_visible(False)
     fig = p.figure()   
     fig.set_facecolor('w')
-    ax = fig.add_subplot(1,1,1)
+    ax = fig.add_subplot(1, 1, 1)
     ax.set_title('')
-    ax.spines['left'].set_position(('data',0))
+    ax.spines['left'].set_position(('data', 0))
     ax.spines['right'].set_color('none')
-    ax.spines['bottom'].set_position(('data',0))
+    ax.spines['bottom'].set_position(('data', 0))
     ax.spines['top'].set_color('none')
     ax.spines['left'].set_smart_bounds(True)
     ax.spines['bottom'].set_smart_bounds(True)
@@ -154,11 +154,11 @@ if __name__ == '__main__':
 
     # constant 'thickness':
     #
-    n_layers_list  = [   12]
+    n_layers_list = [   12]
     thickness_list = [ 0.06]
 #    n_layers_list  = [   4,    8,    12,   16,   20]
 #    thickness_list = [0.06, 0.06,  0.06, 0.06, 0.06]
-    zip_list = zip( n_layers_list, thickness_list  )
+    zip_list = zip(n_layers_list, thickness_list)
     
 #    
     # constant 'rho_tex':
@@ -198,7 +198,7 @@ if __name__ == '__main__':
 #        # possible options: 'bilinear','block','quadratic'
 #        #
 #                
-        for  sig_c_config in ['bilinear','block','quadratic']:   
+        for  sig_c_config in ['bilinear', 'block', 'quadratic']:   
             sig_fl_calib.sig_c_config = sig_c_config
             sig_fl_calib.calib_config = 'cubic'
             sig_fl_calib.calib_sig_t_mfn()
@@ -236,7 +236,7 @@ if __name__ == '__main__':
             
             # reproduce the forces for the calibration test:
             #
-            eps_lo =   sig_fl_calib.eps_t_fail
+            eps_lo = sig_fl_calib.eps_t_fail
             
             
         
@@ -253,18 +253,18 @@ if __name__ == '__main__':
             ##########################################################################
             #mainly compression strain:
             
-            if  sig_fl_calib.sig_c_config== 'quadratic_2':    
-                eps_compression = -(2. + 0.085 * ((f_ck+8.) - 50)**0.53) / 1000.  #eps_c2
+            if  sig_fl_calib.sig_c_config == 'quadratic_2':    
+                eps_compression = -(2. + 0.085 * ((f_ck + 8.) - 50) ** 0.53) / 1000.  #eps_c2
                 
-            elif sig_fl_calib.sig_c_config== 'bilinear':   
+            elif sig_fl_calib.sig_c_config == 'bilinear':   
             
                 eps_compression = -(1.75 + 0.55 * (((f_ck + 8) - 50.) / 40.)) / 1000. #eps_c3
                 
-            elif sig_fl_calib.sig_c_config== 'quadratic':   
+            elif sig_fl_calib.sig_c_config == 'quadratic':   
                 
-                eps_compression = -min(0.7 *( f_ck + 8. ) ** 0.31, 2.8) / 1000. #eps_c1
+                eps_compression = -min(0.7 * (f_ck + 8.) ** 0.31, 2.8) / 1000. #eps_c1
                 
-            elif sig_fl_calib.sig_c_config== 'block':   
+            elif sig_fl_calib.sig_c_config == 'block':   
                 
                 eps_compression = -0.002
             
@@ -272,41 +272,41 @@ if __name__ == '__main__':
        
           
             
-            eps_c_fail = 3.3/1000.
+            eps_c_fail = 3.3 / 1000.
             eps_t_fail = sig_fl_calib.eps_t_fail
-            print'eps_t_fail ',eps_t_fail
+            print'eps_t_fail ', eps_t_fail
             # mainly tension
             
-            eps_lo_arr_A1 = np.linspace( eps_t_fail,   eps_t_fail, n_A )
-            eps_up_arr_A1 = np.linspace( eps_t_fail,            0, n_A )
+            eps_lo_arr_A1 = np.linspace(eps_t_fail, eps_t_fail, n_A)
+            eps_up_arr_A1 = np.linspace(eps_t_fail, 0, n_A)
             
             #  flexion2
             
-            eps_lo_arr_A2 = np.linspace(eps_t_fail,   eps_t_fail, n_A )
-            eps_up_arr_A2 = np.linspace(         0, - eps_c_fail, n_A )
+            eps_lo_arr_A2 = np.linspace(eps_t_fail, eps_t_fail, n_A)
+            eps_up_arr_A2 = np.linspace(0, -eps_c_fail, n_A)
             
             #  flexion3
             
-            eps_lo_arr_B = np.linspace( eps_t_fail,           0., n_B )
-            eps_up_arr_B = np.linspace(-eps_c_fail, - eps_c_fail, n_B )
+            eps_lo_arr_B = np.linspace(eps_t_fail, 0., n_B)
+            eps_up_arr_B = np.linspace(-eps_c_fail, -eps_c_fail, n_B)
             
             # pure compression
             
-            eps_lo_arr_C = np.linspace(         0.,       eps_compression, n_C )
-            eps_up_arr_C = np.linspace(-eps_c_fail,       eps_compression, n_C )
+            eps_lo_arr_C = np.linspace(0., eps_compression, n_C)
+            eps_up_arr_C = np.linspace(-eps_c_fail, eps_compression, n_C)
             
             # plot stress cases
             
-            eps_lo_arr_psc = np.array  ([ 0.,          0., 0.,    eps_t_fail, 0. ,eps_t_fail, 0.])
-            eps_up_arr_psc = np.array  ([ 0., -eps_c_fail, 0.,   -eps_c_fail, 0. ,        0., 0.])
+            eps_lo_arr_psc = np.array  ([ 0., 0., 0., eps_t_fail, 0. , eps_t_fail, 0.])
+            eps_up_arr_psc = np.array  ([ 0., -eps_c_fail, 0., -eps_c_fail, 0. , 0., 0.])
         
         #    all stress cases with classification of stress cases
         
 #            eps_lo_arr = np.hstack([ eps_lo_arr_A1,eps_lo_arr_A2, eps_lo_arr_B, eps_lo_arr_C,eps_lo_arr_psc ])
 #            eps_up_arr = np.hstack([ eps_up_arr_A1,eps_up_arr_A2, eps_up_arr_B, eps_up_arr_C,eps_up_arr_psc ])
         #    all stress cases without classification of stress cases    
-            eps_lo_arr = np.hstack([ eps_lo_arr_A1,eps_lo_arr_A2, eps_lo_arr_B, eps_lo_arr_C ])
-            eps_up_arr = np.hstack([ eps_up_arr_A1,eps_up_arr_A2, eps_up_arr_B, eps_up_arr_C ])
+            eps_lo_arr = np.hstack([ eps_lo_arr_A1, eps_lo_arr_A2, eps_lo_arr_B, eps_lo_arr_C ])
+            eps_up_arr = np.hstack([ eps_up_arr_A1, eps_up_arr_A2, eps_up_arr_B, eps_up_arr_C ])
 ##            
             psc = 'True'
             eps_lo_arr_psc = ([ eps_lo_arr_psc])
@@ -336,10 +336,10 @@ if __name__ == '__main__':
             sig_fl_calib.n_layers = n        
             sig_fl_calib.thickness = t
             #
-            eval_N_M_vectorized = np.frompyfunc( sig_fl_calib.eval_N_M, 2, 2 )
-            N_arr, M_arr = eval_N_M_vectorized( eps_lo_arr, eps_up_arr )
+            eval_N_M_vectorized = np.frompyfunc(sig_fl_calib.eval_N_M, 2, 2)
+            N_arr, M_arr = eval_N_M_vectorized(eps_lo_arr, eps_up_arr)
             
-            N_arr_psc, M_arr_psc = eval_N_M_vectorized( eps_lo_arr_psc, eps_up_arr_psc )
+            N_arr_psc, M_arr_psc = eval_N_M_vectorized(eps_lo_arr_psc, eps_up_arr_psc)
             
             #
             # @todo: check which value to be used for normalization if sig_c = parabolic is used
@@ -347,8 +347,8 @@ if __name__ == '__main__':
             # which does not yield f_cm in the parabolic law; for the default concrete laws for pure
             # compression the value should evaluated to 1.0!
             #
-            c1 = 1000. * t* b * f_ck
-            c2 = 1000. * t**2. * b * f_ck 
+            c1 = 1000. * t * b * f_ck
+            c2 = 1000. * t ** 2. * b * f_ck 
             #
             # @todo: why does an error occure when multiplying an array by a float value (caused by vectorized)
             nu_arr = N_arr / np.array([ c1 ]) 
@@ -356,64 +356,64 @@ if __name__ == '__main__':
             
             nu_arr_psc = N_arr_psc / np.array([ c1 ]) 
             mu_arr_psc = M_arr_psc / np.array([ c2 ])
-            print 'mu_arr_psc',mu_arr_psc
+            print 'mu_arr_psc', mu_arr_psc
             
-            print 'nu_arr_psc',nu_arr_psc
+            print 'nu_arr_psc', nu_arr_psc
             rho_tex = n * A_roving * n_rovings / c1
             #
-            N_list.append( N_arr )
-            M_list.append( M_arr )
-            nu_list.append( nu_arr )
-            mu_list.append( mu_arr )
-            rho_list.append( rho_tex )
+            N_list.append(N_arr)
+            M_list.append(M_arr)
+            nu_list.append(nu_arr)
+            mu_list.append(mu_arr)
+            rho_list.append(rho_tex)
             
     
-            if psc=='True':
-                p.plot (mu_arr_psc, nu_arr_psc, 'k--',
-                                       color='black', 
-                                      linewidth=2.0)
-#    
+            if psc == 'True':
+                p.plot (mu_arr_psc[0, :], nu_arr_psc[0, :], 'k--',
+                                       color = 'black',
+                                      linewidth = 2.0)
+    
             if t == 0.06 and n == 12:
                 
-                if sig_fl_calib.calib_config =='linear':
-                    p.plot( mu_arr, nu_arr, 
+                if sig_fl_calib.calib_config == 'linear':
+                    p.plot(mu_arr, nu_arr,
                             #color='blue', 
-                            linewidth=2.0)
+                            linewidth = 2.0)
                 elif sig_fl_calib.calib_config == 'cubic':  
-                    p.plot(mu_arr, nu_arr, 
+                    p.plot(mu_arr, nu_arr, label = 'cubic',
                            # color='red', 
-                            linewidth=2.0)
+                            linewidth = 2.0)
                     
                 elif sig_fl_calib.calib_config == 'bilinear':  
-                    p.plot( mu_arr, nu_arr, 
-                            color='purple', 
-                            linewidth=2.0)
+                    p.plot(mu_arr, nu_arr,
+                            color = 'purple',
+                            linewidth = 2.0)
                     
                 elif sig_fl_calib.calib_config == 'fbm':  
-                    p.plot( mu_arr, nu_arr, 
-                            color='turquoise', 
-                            linewidth=2.0)
+                    p.plot(mu_arr, nu_arr,
+                            color = 'turquoise',
+                            linewidth = 2.0)
                          
             else:
                 
-                if sig_fl_calib.calib_config =='linear':
-                    p.plot( mu_arr, nu_arr,  
+                if sig_fl_calib.calib_config == 'linear':
+                    p.plot(mu_arr, nu_arr,
                             #color='blue', 
-                            linewidth=1.0)
+                            linewidth = 1.0)
                 elif sig_fl_calib.calib_config == 'cubic':  
-                    p.plot(mu_arr, nu_arr, 
-                            color='red', 
-                            linewidth=1.0)
+                    p.plot(mu_arr, nu_arr,
+                            color = 'red',
+                            linewidth = 1.0)
                     
                 elif sig_fl_calib.calib_config == 'bilinear':  
-                    p.plot( mu_arr, nu_arr, 
-                            color='purple',
-                            linewidth=1.0)
+                    p.plot(mu_arr, nu_arr,
+                            color = 'purple',
+                            linewidth = 1.0)
                     
                 elif sig_fl_calib.calib_config == 'fbm':  
-                    p.plot( mu_arr, nu_arr, 
-                            color='turquoise', 
-                            linewidth=1.0)
+                    p.plot(mu_arr, nu_arr,
+                            color = 'turquoise',
+                            linewidth = 1.0)
     #    
             # plot absolute values for N and M
         #        
@@ -471,7 +471,7 @@ if __name__ == '__main__':
     
         ### GRID
         #
-        p.grid(b=None, which='major')
+        p.grid(b = None, which = 'major')
     
     
         
@@ -481,28 +481,28 @@ if __name__ == '__main__':
 #        legend_list = [0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.] 
 #        legend_list = thickness_list 
 #        legend_list = ['linear','cubic','fbm','bilinear']
-        legend_list = ['bilinear','block','quadratic']
+#        legend_list = ['bilinear', 'block', 'quadratic']
 #        legend_list = n_layers_list 
 ###################################################################################
-        p.legend( legend_list, 'lower right', shadow=True )
+        p.legend() # 'lower right', shadow = True)
         
     
     
         ### TICKS
         #
     #    p.axis([0, 8, 250 ,-900])
-        p.axis([0, 0.15, 0.5 ,-1.25])
+        p.axis([0, 0.15, 0.5 , -1.25])
         #p.xticks(0.5*np.arange(1.2))
     #    ax.set_xticks([0., 1., 2., 3., 4., 5., 6.])
     #    ax.set_ylim(ax.get_ylim()[::-1])
         
         ax.xaxis.set_major_locator(MaxNLocator(10))
         ax.yaxis.set_major_locator(MaxNLocator(10))
-        minorLocator   = AutoMinorLocator()
+        minorLocator = AutoMinorLocator()
         ax.xaxis.set_minor_locator(minorLocator)
 
-        ax.tick_params(axis='both',which='major',direction='out', length=6, width=2, colors='black')
-        ax.tick_params(axis='both',which='minor',direction='out', length=0, width=0, colors='black')
+        ax.tick_params(axis = 'both', which = 'major', direction = 'out', length = 6, width = 2, colors = 'black')
+        ax.tick_params(axis = 'both', which = 'minor', direction = 'out', length = 0, width = 0, colors = 'black')
     
     
         ### TITEL
@@ -514,9 +514,9 @@ if __name__ == '__main__':
         
         ### LABELS
         
-        p.xlabel(r'$\mu$',fontsize='20',
+        p.xlabel(r'$\mu$', fontsize = '20',
                  verticalalignment = 'top',
                  horizontalalignment = 'left')
-        p.ylabel(r'$\nu$ ', fontsize= 20)
+        p.ylabel(r'$\nu$ ', fontsize = 20)
 
         p.show()
