dadcb4aee6246a0dde8add6c0428700f7ab8d562
ale-x <ascholzen@imb.rwth-aachen.de>
2012-12-12 16:00:36 +0100

    fabric layout for AR-glass tissue binding added (barrel shell)

--- matresdev/matresdev/db/matdb/trc/fabric_layout.py
+++ matresdev/matresdev/db/matdb/trc/fabric_layout.py
@@ -86,8 +86,8 @@ FabricLayOut.db = SimDBClassExt(
                'MAG-07-03' : FabricLayOut( 
                                            a_tex_0 = 107.89,
                                            a_tex_90 = 106.61,
-                                           E_tex_0 = 70000,
-                                           E_tex_90 = 70000,
+                                           E_tex_0 = 70000.,
+                                           E_tex_90 = 70000.,
                                            s_tex_0 = 8.3,
                                            s_tex_90 = 8.4,
                                            ),
@@ -97,8 +97,8 @@ FabricLayOut.db = SimDBClassExt(
                '2D-15-10' : FabricLayOut( 
                                            a_tex_0 = 107.89,
                                            a_tex_90 = 106.61,
-                                           E_tex_0 = 70000,
-                                           E_tex_90 = 70000,
+                                           E_tex_0 = 70000.,
+                                           E_tex_90 = 70000.,
                                            s_tex_0 = 8.3,
                                            s_tex_90 = 8.4,
                                            ),
@@ -109,8 +109,8 @@ FabricLayOut.db = SimDBClassExt(
                '2D-02-06a' : FabricLayOut( 
                                            a_tex_0 = 71.65,
                                            a_tex_90 = 53.31,
-                                           E_tex_0 = 70000,
-                                           E_tex_90 = 70000,
+                                           E_tex_0 = 70000.,
+                                           E_tex_90 = 70000.,
                                            s_tex_0 = 12.5,
                                            s_tex_90 = 8.4,
                                            ),
@@ -122,8 +122,8 @@ FabricLayOut.db = SimDBClassExt(
                '2D-14-10' : FabricLayOut( 
                                            a_tex_0 = 73.89,
                                            a_tex_90 = 60., #55.0 * 8.4mm / 7.7mm
-                                           E_tex_0 = 165000,
-                                           E_tex_90 = 165000,
+                                           E_tex_0 = 165000.,
+                                           E_tex_90 = 165000.,
                                            s_tex_0 = 12.5,
                                            s_tex_90 = 5.83, # input for manufacturing machine was 8.4 mm!
                                            ),
@@ -135,8 +135,8 @@ FabricLayOut.db = SimDBClassExt(
                '2D-04-11' : FabricLayOut( 
                                            a_tex_0 = 55.4,
                                            a_tex_90 = 60., #55.0 * 8.4mm / 7.7mm
-                                           E_tex_0 = 165000,
-                                           E_tex_90 = 165000,
+                                           E_tex_0 = 165000.,
+                                           E_tex_90 = 165000.,
                                            s_tex_0 = 8.3,
                                            s_tex_90 = 7.6, # input for manufacturing machine was 8.4 mm!
                                            ),
@@ -149,13 +149,13 @@ FabricLayOut.db = SimDBClassExt(
                '2D-04-11_defect' : FabricLayOut( 
                                            a_tex_0 = 55.4,
                                            a_tex_90 = 60., #55.0 * 8.4mm / 7.7mm
-                                           E_tex_0 = 165000,
-                                           E_tex_90 = 165000,
+                                           E_tex_0 = 165000.,
+                                           E_tex_90 = 165000.,
                                            s_tex_0 = 8.3,
                                            s_tex_90 = 7.6, # input for manufacturing machine was 8.4 mm!
                                            ),
                
-               # demonstrator textile
+               # demonstrator textile (carbon)
                # carbon textile / tissue binding ("Tuch") 
                # 1 x 800 tex in 0-direction (1v1l): 8.3 mm                            
                # 1 x 800 tex in 90-direction (1v1l): effective spacing of 7.6 mm!                            
@@ -163,11 +163,27 @@ FabricLayOut.db = SimDBClassExt(
                '2D-05-11' : FabricLayOut( 
                                            a_tex_0 = 55.4,
                                            a_tex_90 = 60., #55.0 * 8.4mm / 7.7mm
-                                           E_tex_0 = 165000,
-                                           E_tex_90 = 165000,
+                                           E_tex_0 = 165000.,
+                                           E_tex_90 = 165000.,
                                            s_tex_0 = 8.3,
                                            s_tex_90 = 7.6, # input for manufacturing machine was 8.4 mm!
                                            ),
+
+               # AR-glass tissue binding (barrel shell)
+               # carbon textile / tissue binding ("Tuch") 
+               # 1 x 1200 tex in 0-direction (1v1l): 8.3 mm                            
+               # 1 x 1200 tex in 90-direction (1v1l): effective spacing of 7.6 mm!                            
+               #
+               '2D-09-12' : FabricLayOut( 
+                                           a_tex_0 = 55.4,
+                                           a_tex_90 = 60., #55.0 * 8.4mm / 7.7mm
+                                           E_tex_0 = 70000.,
+                                           E_tex_90 = 70000.,
+                                           s_tex_0 = 8.3,
+                                           s_tex_90 = 7.6, # input for manufacturing machine was 8.4 mm!
+                                           ),
+
+
 
 
                # carbon textile (heavy tow 3300 tex): Trikot binding       
@@ -176,8 +192,8 @@ FabricLayOut.db = SimDBClassExt(
                '2D-18-10' : FabricLayOut( 
                                            a_tex_0 = 76.96,
                                            a_tex_90 = 110,
-                                           E_tex_0 = 107500,
-                                           E_tex_90 = 107500,
+                                           E_tex_0 = 107500.,
+                                           E_tex_90 = 107500.,
                                            s_tex_0 = 25.0,
                                            s_tex_90 = 16.8,
                                            ),
@@ -187,8 +203,8 @@ FabricLayOut.db = SimDBClassExt(
                'C-Grid-C50' : FabricLayOut( 
                                            a_tex_0 = 37.53,
                                            a_tex_90 = 42.03,
-                                           E_tex_0 = 234500,
-                                           E_tex_90 = 234500,
+                                           E_tex_0 = 234500.,
+                                           E_tex_90 = 234500.,
                                            s_tex_0 = 46.0,
                                            s_tex_90 = 41.0,
                                            )
