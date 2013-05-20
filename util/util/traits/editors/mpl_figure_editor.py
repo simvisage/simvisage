af8d395fead8ee67097256e95a9a420b3459e52a
kelidas <kelidas@kcentrum.cz>
2013-05-20 10:58:35 +0200

    new functionality added; status bar added and key-bindings (l,k) to change axis scale (normal -> log)

--- util/util/traits/editors/mpl_figure_editor.py
+++ util/util/traits/editors/mpl_figure_editor.py
@@ -5,7 +5,7 @@ import matplotlib
 matplotlib.use('WXAgg')
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx, StatusBarWx
 
 from etsproxy.traits.api import Any, Instance
 #from etsproxy.traits.ui.api import Editor
@@ -24,22 +24,49 @@ class _MPLFigureEditor(Editor):
 
     def update_editor(self):
         figure = self.value
+        figure.canvas.mpl_connect('key_press_event', self.key_press_callback)
         figure.canvas.draw()
 
     def _create_canvas(self, parent):
         """ Create the MPL canvas. """
         # The panel lets us add additional controls.
-        panel = wx.Panel(parent, -1, style = wx.CLIP_CHILDREN)
+        panel = wx.Panel(parent, -1, style=wx.CLIP_CHILDREN)
         sizer = wx.BoxSizer(wx.VERTICAL)
         panel.SetSizer(sizer)
 
         # matplotlib commands to create a canvas
-        mpl_control = FigureCanvas(panel, -1, self.value)
+        mpl_control = FigureCanvas(panel, 1, self.value)
+        statbar = StatusBarWx(panel)
         toolbar = NavigationToolbar2Wx(mpl_control)
+        toolbar.set_status_bar(statbar)
+
         sizer.Add(toolbar, 0, wx.EXPAND)
-        sizer.Add(mpl_control, 1, wx.LEFT | wx.TOP | wx.GROW)
+        sizer.Add(mpl_control, 1, wx.EXPAND)
+
+        sizer.Add(toolbar.statbar, 0, wx.EXPAND)
         self.value.canvas.SetMinSize((100, 100))
         return panel
+
+    # TODO: generalize
+    def key_press_callback(self, event):
+        'whenever a key is pressed'
+        figure = self.value
+        if not event.inaxes: return
+        if event.key == 'k':
+            if figure.axes[0].get_xscale() == 'log':
+                figure.axes[0].set_xscale('linear')
+                figure.canvas.draw()
+            else:
+                figure.axes[0].set_xscale('log')
+                figure.canvas.draw()
+
+        if event.key == 'l':
+            if figure.axes[0].get_yscale() == 'log':
+                figure.axes[0].set_yscale('linear')
+                figure.canvas.draw()
+            else:
+                figure.axes[0].set_yscale('log')
+                figure.canvas.draw()
 
 class MPLFigureEditor(BasicEditorFactory):
 
@@ -48,19 +75,19 @@ class MPLFigureEditor(BasicEditorFactory):
 
 if __name__ == "__main__":
     # Create a window to demo the editor
-    from etsproxy.traits.api import HasTraits
-    from etsproxy.traits.ui.api import View, Item
+    from traits.api import HasTraits
+    from traitsui.api import View, Item
     from numpy import sin, cos, linspace, pi
 
     class Test(HasTraits):
 
         figure = Instance(Figure, ())
 
-        view = View(Item('figure', editor = MPLFigureEditor(),
-                                show_label = False),
-                        width = 400,
-                        height = 300,
-                        resizable = True)
+        view = View(Item('figure', editor=MPLFigureEditor(),
+                                show_label=False),
+                        width=400,
+                        height=300,
+                        resizable=True)
 
         def __init__(self):
             super(Test, self).__init__()
