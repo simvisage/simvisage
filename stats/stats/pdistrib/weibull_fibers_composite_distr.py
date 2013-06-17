433d306ff087ef3501f37aa1edb859dfa365c1ae
rostar <rrypl@.imb.rwth-aachen.de>
2013-06-17 11:05:00 +0200

    namespace adapted

--- stats/stats/pdistrib/weibull_fibers_composite_distr.py
+++ stats/stats/pdistrib/weibull_fibers_composite_distr.py
@@ -9,6 +9,7 @@ from scipy.special import gamma
 import numpy as np
 from math import pi
 
+
 def H(x):
     return x >= 0.0
 
@@ -23,14 +24,15 @@ class WeibullFibers(HasTraits):
 
     def mean(self, depsf, r):
         m = self.shape
-        s = (depsf * (m + 1) * self.sV0 ** m * self.V0 / 2 / pi / r **2)**(1./(m+1))
-        return s * gamma(1. + 1/(m+1.))
+        s = (depsf * (m + 1) * self.sV0 ** m * self.V0 / 2 / pi / r ** 2) ** (1. / (m + 1))
+        return s * gamma(1. + 1 / (m + 1.))
 
-    def weibull_fibers_Pf(self, epsy_arr, depsf, x_short, x_long, r_arr):
+    def cdf(self, max_strain, strain_slope, shorter_boundary, longer_boundary, fiber_radius):
         m = self.shape
-        x_short = np.hstack((x_short[1:], np.repeat(x_short[-1], len(epsy_arr)-len(x_short[1:]))))
-        x_long = np.hstack((x_long[1:], np.repeat(x_long[-1], len(epsy_arr)-len(x_long[1:]))))
-        s = depsf * (m + 1) * self.sV0 ** m * self.V0 / pi / r_arr **2
-        a0 = epsy_arr / depsf
-        Pf = 1. - np.exp( - epsy_arr ** (m+1)/s * (2. - (1-x_short/a0)**(m+1) - (1-x_long/a0)**(m+1)) )
-        return Pf * H(epsy_arr)
+        x_short = np.hstack((shorter_boundary[1:], np.repeat(shorter_boundary[-1], len(max_strain)-len(shorter_boundary[1:]))))
+        x_long = np.hstack((longer_boundary[1:], np.repeat(longer_boundary[-1], len(max_strain)-len(longer_boundary[1:]))))
+        s = strain_slope * (m + 1) * self.sV0 ** m * self.V0 / pi / fiber_radius ** 2
+        a0 = max_strain / strain_slope
+        Pf = 1. - np.exp(- max_strain ** (m + 1) / s *
+            (2. - (1 - x_short / a0) ** (m + 1) - (1 - x_long / a0) ** (m + 1)))
+        return Pf * H(max_strain)
