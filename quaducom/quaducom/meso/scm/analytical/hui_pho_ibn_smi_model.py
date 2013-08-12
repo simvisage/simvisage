b36679afaaf19d951cd10dc5fbfe737819a6e186
rostar <rrypl@.imb.rwth-aachen.de>
2013-08-12 15:52:37 +0200

    started hiu phoenix ibn smith model

---
+++ quaducom/quaducom/meso/scm/analytical/hui_pho_ibn_smi_model.py
@@ -0,0 +1,13 @@
+'''
+Created on Apr 20, 2012
+
+model of single fiber composite by Hui, Phoenix, Ibnabeljalil and Smith
+
+@author: rostar
+'''
+
+from etsproxy.traits.api import HasTraits, Float, Property, cached_property
+
+class SFC_Hui(HasTraits):
+    
+    
