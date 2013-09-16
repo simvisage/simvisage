dc0f3817e0e635c1e81708e1b135a87aa7fc0cfe
rostar <rrypl@.imb.rwth-aachen.de>
2013-09-16 14:42:17 +0200

    fiber pullout included

--- stats/stats/pdistrib/weibull_fibers_composite_distr.py
+++ stats/stats/pdistrib/weibull_fibers_composite_distr.py
@@ -5,13 +5,13 @@ Created on Jun 6, 2013
 '''
 
 from etsproxy.traits.api import HasTraits, Float
-from scipy.stats import weibull_min
 import numpy as np
 from math import pi
+from scipy.special import gamma, gammainc
 
 
 def H(x):
-    return x >= 0.0 
+    return x >= 0.0
 
 
 class WeibullFibers(HasTraits):
@@ -22,20 +22,27 @@ class WeibullFibers(HasTraits):
     sV0 = Float
     V0 = Float(1.)
 
-    def weibull_fibers_cdf(self, max_strain, strain_slope,
-                           shorter_boundary, longer_boundary, fiber_radius):
+    def weibull_fibers_cdf(self, ef0, strain_slope,
+                           shorter_boundary, longer_boundary, r):
         m = self.shape
 #        Ll = shorter_boundary
 #        Lr = longer_boundary
-#        if np.any(Lr > max_strain / strain_slope + 1e-15):
+#        if np.any(Lr > ef0 / strain_slope + 1e-15):
 #            print 'wrong input for Pf (WeibullFibers)'
-#        s = (strain_slope ** 2 * (m + 1) * self.sV0 ** m * self.V0 / pi / fiber_radius ** 2) ** (1./(m+2))
-#        a0 = max_strain / strain_slope + 1e-15
-#        Pf = 1. - np.exp(- (max_strain / s ) ** (m + 2) *
+#        s = (strain_slope ** 2 * (m + 1) * self.sV0 ** m * self.V0 / pi / r ** 2) ** (1./(m+2))
+#        a0 = ef0 / strain_slope + 1e-15
+#        Pf = 1. - np.exp(- (ef0 / s ) ** (m + 2) *
 #            ((1. - (1. - Ll / a0) ** (m + 1))/Ll   +  (1. - (1. - Lr / a0) ** (m + 1))/Lr))
-#        return Pf * H(max_strain)
-        a = 2 * max_strain / strain_slope
-        return weibull_min(m, scale=(self.V0 * self.sV0 ** m / fiber_radius ** 2 / pi / a)**(1./m)).cdf(max_strain)
+#        return Pf * H(ef0)
+
+        s = ((strain_slope * (m+1) * self.sV0**m)/(2. * pi * r ** 2))**(1./(m+1))
+        Pf_int = 1 - np.exp(-(ef0/s)**(m+1))
+        I = s * gamma(1 + 1./(m+1)) * gammainc(1 + 1./(m+1), (ef0/s)**(m+1))
+        Pf_broken = I / (m + 1) / ef0
+        return Pf_int - Pf_broken
+
+#        a = 2 * max_strain / strain_slope
+#        return weibull_min(m, scale=(self.V0 * self.sV0 ** m / fiber_radius ** 2 / pi / a)**(1./m)).cdf(max_strain)
 
 if __name__ == '__main__':
     from matplotlib import pyplot as plt
