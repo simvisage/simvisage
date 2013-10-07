c67e5b5d7dcc536e8b24b5729f401f733a85c240
rostar <rrypl@.imb.rwth-aachen.de>
2013-10-07 17:31:29 +0200

    params

--- quaducom/quaducom/meso/homogenized_crack_bridge/elastic_matrix/hom_CB_elastic_mtrx_view.py
+++ quaducom/quaducom/meso/homogenized_crack_bridge/elastic_matrix/hom_CB_elastic_mtrx_view.py
@@ -213,7 +213,7 @@ if __name__ == '__main__':
     from stats.pdistrib.weibull_fibers_composite_distr import WeibullFibers, fibers_MC
 
     reinf = ContinuousFibers(r=0.0035,
-                          tau=RV('weibull_min', loc=0.006, shape=.23, scale=0.03),
+                          tau=RV('weibull_min', loc=0.006, shape=2.23, scale=0.03),
                           V_f=0.04,
                           E_f=240e3,
                           xi=fibers_MC(m=5.0, sV0=0.0026),
@@ -280,7 +280,7 @@ if __name__ == '__main__':
     # energy(np.linspace(.0, .15, 100))
 #    sigma_c = np.linspace(1., 7., 7)
     #profile(0.031)
-    w = np.linspace(0., .04, 200)
+    w = np.linspace(0.0, .04, 200)
     sigma_c_w(w)
     #energy(w)
     # bundle at 20 mm
