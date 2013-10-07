30e3c9a5aa93eebabc21514b58eb7b8b16a50ca8
rostar <rrypl@.imb.rwth-aachen.de>
2013-10-07 11:47:10 +0200

    weibull fibers failure for single and multiple cracking implemented and CB module adapted

--- stats/stats/pdistrib/weibull_fibers_composite_distr.py
+++ stats/stats/pdistrib/weibull_fibers_composite_distr.py
@@ -33,11 +33,7 @@ class fibers_dry(WeibullFibers):
     def cdf(self, e, r, L):
         s = (self.sV0 ** self.m * self.V0 / (L * r ** 2 * pi)) ** (1./self.m)
         return 1. - np.exp(- (e/s)**self.m)
-    
-    def pdf(self, e, r, L):
-        s = (self.sV0 ** self.m * self.V0 / (L * r ** 2 * pi)) ** (1./self.m)
-        return self.m / s * (e / s)**(self.m - 1.) * np.exp(- (e/s)**self.m)
-    
+
 
 class fibers_CB_rigid(WeibullFibers):
     ''' distribution of strength of brittle fibers-in-composite with flaws following
@@ -51,18 +47,13 @@ class fibers_CB_rigid(WeibullFibers):
                             tau = bond strength
                             Ef = fiber modulus of elasticity'''
 
-    def cdf(self, e, r, tau, Ef):
+    def cdf(self, e, depsf, r):
+        '''weibull_fibers_cdf_cb_rigid'''
         m = self.m
-        T = 2. * tau / r
-        s = ((T*(m+1)*self.sV0**m*self.V0)/(2.*Ef*pi*r**2))**(1./(m+1))
+        s = ((depsf*(m+1)*self.sV0**m*self.V0)/(2.*pi*r**2))**(1./(m+1))
         return 1. - np.exp(-(e/s) ** (m + 1))
 
-    def pdf(self, e, r, tau, Ef):
-        m = self.m
-        T = 2. * tau / r
-        s = ((T*(m+1.)*self.sV0**m*self.V0)/(2.*Ef*pi*r**2.))**(1./(m+1.))
-        return (m+1.)/s * (e/s)**m * np.exp(- (e/s)**(m+1.))
-    
+
 class fibers_CB_elast(WeibullFibers):
     ''' distribution of strength of brittle fibers-in-composite with flaws following
         the compound Poisson process.
@@ -76,23 +67,14 @@ class fibers_CB_elast(WeibullFibers):
                             Ef = fiber modulus of elasticity
                             a = debonded length'''
 
-    def cdf(self, e, r, tau, Ef, a):
+    def cdf(self, e, depsf, r, al, ar):
+        '''weibull_fibers_cdf_cb_elast'''
         m = self.m
-        T = 2. * tau / r
-        s = ((T*(m+1.)*self.sV0**m*self.V0)/(2.*Ef*pi*r**2.))**(1./(m+1.))
-        a0 = e/(T/Ef)
-        a = a0 * 0.1
-        return 1. - np.exp(-(e/s) ** (m + 1) * (1.-(1.-a/a0)**(m+1.)))
-
-    def pdf(self, e, r, tau, Ef, a):
-        m = self.m
-        T = 2. * tau / r
-        s = ((T*(m+1.)*self.sV0**m*self.V0)/(2.*Ef*pi*r**2.))**(1./(m+1.))
-        a0 = e/(T/Ef)
-        a = a0 * 0.9
-        part1 = ((m+1.)*(e/s) ** (m + 1) * (1.-(1.-a/a0)**(m+1.))/e - a * T *(m+1.)*(e/s) ** (m + 1) * (1.-a/a0)**(m+1.)/e**2/Ef )
-        part2 = np.exp(-(e/s) ** (m + 1) * (1.-(1.-a/a0)**(m+1.)))
-        return part1 * part2
+        s = ((depsf*(m+1.)*self.sV0**m*self.V0)/(pi*r**2.))**(1./(m+1.))
+        a0 = e/depsf
+        expL = (e/s) ** (m + 1) * (1.-(1.-al/a0)**(m+1.))
+        expR = (e/s) ** (m + 1) * (1.-(1.-ar/a0)**(m+1.))
+        return 1. - np.exp(-expL-expR)
 
 
 class fibers_MC(WeibullFibers):
@@ -110,53 +92,53 @@ class fibers_MC(WeibullFibers):
                             Ll = symmetry condition left
                             Lr = symmetry condition right'''
 
-    def cdf(self, e, r, tau, Ef, Ll, Lr):
-        print 'if a0 < Ll'
-        m = self.m
-        T = 2. * tau / r
-        s = ((T*(m+1.)*self.sV0**m*self.V0)/(Ef*pi*r**2.))**(1./(m+1.))
-        a0 = e/(T/Ef)
-        expL = a0 / Ll * (e/s) ** (m + 1) * (1.-(1.-Ll/a0)**(m+1.))
-        expR = a0 / Lr * (e/s) ** (m + 1) * (1.-(1.-Lr/a0)**(m+1.))
-        return 1. - np.exp(- expL - expR)
+    Ll = Float(1e10)
+    Lr = Float(1e10)
 
-#        Ll = shorter_boundary
-#        Lr = longer_boundary
-#        if np.any(Lr > ef0 / strain_slope + 1e-15):
-#            print 'wrong input for Pf (WeibullFibers)'
-#        s = (strain_slope ** 2 * (m + 1) * self.sV0 ** m * self.V0 / pi / r ** 2) ** (1./(m+2))
-#        a0 = ef0 / strain_slope + 1e-15
-#        Pf = 1. - np.exp(- (ef0 / s ) ** (m + 2) *
-#            ((1. - (1. - Ll / a0) ** (m + 1))/Ll   +  (1. - (1. - Lr / a0) ** (m + 1))/Lr))
-#        return Pf * H(ef0)
-#        a = 2 * max_strain / strain_slope
-#        return weibull_min(m, scale=(self.V0 * self.sV0 ** m / fiber_radius ** 2 / pi / a)**(1./m)).cdf(max_strain)
+    def cdf(self, e, depsf, r, al, ar):
+        '''weibull_fibers_cdf_mc'''
+        Ll, Lr, m = self.Ll, self.Lr, self.m
+        s = ((depsf*(m+1.)*self.sV0**m*self.V0)/(pi*r**2.))**(1./(m+1.))
+        a0 = e/depsf
+        expLfree = (e/s) ** (m + 1) * (1.-(1.-al/a0)**(m+1.))
+        expLfixed = a0 / Ll * expLfree
+        maskL = al < Ll
+        expL = expLfree * maskL + expLfixed * (maskL == False)
+        expRfree = (e/s) ** (m + 1) * (1.-(1.-ar/a0)**(m+1.))
+        expRfixed = a0 / Lr * expRfree
+        maskR = ar < Lr
+        expR = expRfree * maskR + expRfixed * (maskR == False)
+        return 1. - np.exp(- expL - expR)
         
-    def weibull_fibers_cdf(self, ef0, strain_slope,
-                           shorter_boundary, longer_boundary, r):
-        m = self.shape
-        s = ((strain_slope * (m+1) * self.sV0**m)/(2. * pi * r ** 2))**(1./(m+1))
-        Pf_int = 1 - np.exp(-(ef0/s)**(m+1))
-        I = s * gamma(1 + 1./(m+1)) * gammainc(1 + 1./(m+1), (ef0/s)**(m+1))
-        Pf_broken = I / (m + 1) / ef0
-        return Pf_int# - Pf_broken
+#    def weibull_fibers_cdf(self, ef0, strain_slope,
+#                           shorter_boundary, longer_boundary, r):
+#        m = self.shape
+#        s = ((strain_slope * (m+1) * self.sV0**m)/(2. * pi * r ** 2))**(1./(m+1))
+#        Pf_int = 1 - np.exp(-(ef0/s)**(m+1))
+#        I = s * gamma(1 + 1./(m+1)) * gammainc(1 + 1./(m+1), (ef0/s)**(m+1))
+#        Pf_broken = I / (m + 1) / ef0
+#        return Pf_int - Pf_broken
 
 if __name__ == '__main__':
     from matplotlib import pyplot as plt
     from scipy.integrate import cumtrapz
+    r = 0.00345
+    tau = 0.5
+    Ef = 200e3
+    m = 4.0
+    sV0 = 0.0026
     e = np.linspace(0.001, 0.04, 100)
-    wfcbe = fibers_CB_elast(m=4.0, sV0=0.0026)
-    CDF = wfcbe.cdf(e, 0.00345, 0.5, 200e3, 1.)
+    a = e * Ef / (2. * tau / r)
+    wfcbe = fibers_CB_elast(m=m, sV0=sV0)
+    CDF = wfcbe.cdf(e, 2*tau/Ef/r, r, 0.1 * a, 0.1 * a)
     plt.plot(e, CDF, label='CB')
-    wfcbr = fibers_CB_rigid(m=4.0, sV0=0.0026)
-    CDF = wfcbr.cdf(e, 0.00345, 0.5, 200e3)
+    wfcbr = fibers_CB_rigid(m=m, sV0=sV0)
+    CDF = wfcbr.cdf(e, 2*tau/Ef/r, r)
     plt.plot(e, CDF, label='CB rigid')
-    wfmc = fibers_MC(m=4.0, sV0=0.0026)
-    CDF = wfmc.cdf(e, 0.00345, 0.5, 200e3)
+    wfmc = fibers_MC(m=m, sV0=sV0, Ll=14.0, Lr=14.0)
+    CDF = wfmc.cdf(e, 2*tau/Ef/r, r, a, a)
     plt.plot(e, CDF, label='MC')
     plt.legend()
     plt.show()
-    
-    
-    
+
 
