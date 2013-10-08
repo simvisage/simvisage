b4f5ddfd0cc6fb08103ee9c8b3bb0cfd4cd46912
rostar <erix@post.cz>
2013-10-09 00:48:27 +0200

    cdf MC

--- stats/stats/pdistrib/weibull_fibers_composite_distr.py
+++ stats/stats/pdistrib/weibull_fibers_composite_distr.py
@@ -115,11 +115,11 @@ class fibers_MC(WeibullFibers):
         s = ((depsf*(m+1.)*self.sV0**m*self.V0)/(pi*r**2.))**(1./(m+1.))
         a0 = (e+1e-15)/depsf
         expLfree = (e/s) ** (m + 1) * (1.-(1.-al/a0)**(m+1.))
-        expLfixed = a0 / Ll * expLfree
+        expLfixed = a0 / Ll * (e/s) ** (m + 1) * (1.-(1.-Ll/a0)**(m+1.))
         maskL = al < Ll
         expL = expLfree * maskL + expLfixed * (maskL == False)
         expRfree = (e/s) ** (m + 1) * (1.-(1.-ar/a0)**(m+1.))
-        expRfixed = a0 / Lr * expRfree
+        expRfixed = a0 / Lr * (e/s) ** (m + 1) * (1.-(1.-Lr/a0)**(m+1.))
         maskR = ar < Lr
         expR = expRfree * maskR + expRfixed * (maskR == False)
         return 1. - np.exp(- expL - expR)
@@ -128,6 +128,22 @@ class fibers_MC(WeibullFibers):
         cdf_line = MFnLineArray(xdata=e, ydata=self.cdf(e, depsf, r, al, ar))
         return cdf_line.get_diffs(e)
     
+
+#     def cdf(self, e, depsf, r, al, ar):
+#         '''weibull_fibers_cdf_mc'''
+#         Ll, Lr, m, sV0 = self.Ll, self.Lr, self.m, self.sV0
+#         s = ((depsf*(m+1.)*sV0**m*self.V0)/(pi*r**2.))**(1./(m+1.))
+#         a0 = (e+1e-15)/depsf
+#         expLfree = (e/s) ** (m + 1) * (1.-(1.-al/a0)**(m+1.))
+#         s_fixed = (sV0 ** m * self.V0 / (a0 * r ** 2 * pi)) ** (1./m)
+#         expLfixed = (e/s_fixed)**m
+#         maskL = al < Ll
+#         expL = expLfree * maskL + expLfixed * (maskL == False)
+#         expRfree = (e/s) ** (m + 1) * (1.-(1.-ar/a0)**(m+1.))
+#         expRfixed = (e/s_fixed)**m
+#         maskR = ar < Lr
+#         expR = expRfree * maskR + expRfixed * (maskR == False)
+#         return 1. - np.exp(- expL - expR)
 #    def weibull_fibers_cdf(self, ef0, strain_slope,
 #                           shorter_boundary, longer_boundary, r):
 #        m = self.shape
@@ -148,17 +164,19 @@ if __name__ == '__main__':
     e = np.linspace(0.001, 0.04, 100)
     a = e * Ef / (2. * tau / r)
     wfd = fibers_dry(m=m, sV0=sV0)
-    CDF = wfd.pdf(e, r, 10.)
-    plt.plot(e, CDF, label='dry')
+    CDF = wfd.cdf(e, r, 2*a)
+    #plt.plot(e, CDF, label='dry')
     wfcbe = fibers_CB_elast(m=m, sV0=sV0)
-    CDF = wfcbe.pdf(e, 2*tau/Ef/r, r, 0.1 * a, 0.1 * a)
-    plt.plot(e, CDF, label='CB')
+    CDF = wfcbe.cdf(e, 2*tau/Ef/r, r, 0.1 * a, 0.1 * a)
+    #plt.plot(e, CDF, label='CB')
     wfcbr = fibers_CB_rigid(m=m, sV0=sV0)
-    CDF = wfcbr.pdf(e, 2*tau/Ef/r, r)
-    plt.plot(e, CDF, label='CB rigid')
-    wfmc = fibers_MC(m=m, sV0=sV0, Ll=19.0, Lr=19.0)
-    CDF = wfmc.pdf(e, 2*tau/Ef/r, r, a, a)
+    CDF = wfcbr.cdf(e, 2*tau/Ef/r, r)
+    #plt.plot(e, CDF, label='CB rigid')
+    wfmc = fibers_MC(m=m, sV0=sV0, Ll=1.2, Lr=1.2)
+    CDF = wfmc.cdf(e, 2*tau/Ef/r, r, a, a)
     plt.plot(e, CDF, label='MC')
+    CDF2 = wfmc.cdf2(e, 2*tau/Ef/r, r, a, a)
+    plt.plot(e, CDF2, label='MC2')
     plt.legend()
     plt.show()
 
