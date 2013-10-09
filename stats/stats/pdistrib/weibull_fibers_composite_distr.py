385e3faeac793218df9de062a55c089399af5919
rostar <rrypl@.imb.rwth-aachen.de>
2013-10-09 19:15:24 +0200

    weibull distr MC

--- stats/stats/pdistrib/weibull_fibers_composite_distr.py
+++ stats/stats/pdistrib/weibull_fibers_composite_distr.py
@@ -111,8 +111,8 @@ class fibers_MC(WeibullFibers):
 
     def cdf(self, e, depsf, r, al, ar):
         '''weibull_fibers_cdf_mc'''
-        Ll, Lr, m = self.Ll, self.Lr, self.m
-        s = ((depsf*(m+1.)*self.sV0**m*self.V0)/(pi*r**2.))**(1./(m+1.))
+        Ll, Lr, m, sV0 = self.Ll, self.Lr, self.m, self.sV0
+        s = ((depsf*(m+1.)*sV0**m*self.V0)/(pi*r**2.))**(1./(m+1.))
         a0 = (e+1e-15)/depsf
         expLfree = (e/s) ** (m + 1) * (1.-(1.-al/a0)**(m+1.))
         expLfixed = a0 / Ll * (e/s) ** (m + 1) * (1.-(1.-Ll/a0)**(m+1.))
@@ -157,26 +157,26 @@ if __name__ == '__main__':
     from matplotlib import pyplot as plt
     from scipy.integrate import cumtrapz
     r = 0.00345
-    tau = 0.5
+    tau = 0.1
     Ef = 200e3
-    m = 4.0
+    m = 3.0
     sV0 = 0.0026
-    e = np.linspace(0.001, 0.04, 100)
+    e = np.linspace(0.001, 0.05, 100)
     a = e * Ef / (2. * tau / r)
+    L = 10.0
+    rat = 2.*a/L
     wfd = fibers_dry(m=m, sV0=sV0)
     CDF = wfd.cdf(e, r, 2*a)
-    #plt.plot(e, CDF, label='dry')
+    plt.plot(rat, CDF, label='dry')
     wfcbe = fibers_CB_elast(m=m, sV0=sV0)
     CDF = wfcbe.cdf(e, 2*tau/Ef/r, r, 0.1 * a, 0.1 * a)
     #plt.plot(e, CDF, label='CB')
     wfcbr = fibers_CB_rigid(m=m, sV0=sV0)
     CDF = wfcbr.cdf(e, 2*tau/Ef/r, r)
-    #plt.plot(e, CDF, label='CB rigid')
-    wfmc = fibers_MC(m=m, sV0=sV0, Ll=1.2, Lr=1.2)
+    plt.plot(rat, CDF, label='CB rigid')
+    wfmc = fibers_MC(m=m, sV0=sV0, Ll=L, Lr=L)
     CDF = wfmc.cdf(e, 2*tau/Ef/r, r, a, a)
-    plt.plot(e, CDF, label='MC')
-    CDF2 = wfmc.cdf2(e, 2*tau/Ef/r, r, a, a)
-    plt.plot(e, CDF2, label='MC2')
+    plt.plot(rat, CDF, label='MC')
     plt.legend()
     plt.show()
 
