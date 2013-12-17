663641c08b8e725d4ac10f45978c7f207b3396f7
ale-x <ascholzen@imb.rwth-aachen.de>
2013-12-17 11:09:15 +0100

    add evaluation for eta_nm and interaction plot

--- quaducom/quaducom/assess/assess_shell/lcc_sfbdemonstrator.py
+++ quaducom/quaducom/assess/assess_shell/lcc_sfbdemonstrator.py
@@ -17,10 +17,10 @@ if __name__ == '__main__':
     #                       cut_z_fraction = 0.01,
     #                       cut_z_fraction = 0.05, # corresponds to 50cm x 50cm
     #                       cut_z_fraction = 0.10, # corresponds to 75cm x 75cm
-    cut_z_fraction = 0.15, # corresponds to 100cm x 100cm
+    cut_z_fraction = 0.15,  # corresponds to 100cm x 100cm
 
     def remove_midpoints(lcc_table, arr):
-        # remove the center points of the shells 
+        # remove the center points of the shells
         Zp = np.fabs(lcc_table.geo_data_orig['Z'])
         max_Z = max(Zp)
         min_Z = min(Zp)
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 
 
     #---------------------------------------------
-    # 2 shells: 
+    # 2 shells:
     # new geometry with new loading cases plus waterfilling
     #---------------------------------------------
 
@@ -52,65 +52,65 @@ if __name__ == '__main__':
     #---------------------------------------------------------
     lc_list = [
                  # LC1:
-                 LC(name = 'g', category = 'dead-load', file_name = 'LC1.csv'
+                 LC(name='g', category='dead-load', file_name='LC1.csv'
                     ),
                  # LC2:
-                 LC(name = 's_sym', category = 'imposed-load', file_name = 'LC2.csv',
-                    exclusive_to = ['s_asym', 'WF'],
-                     psi_0 = 0.5, psi_1 = 0.2, psi_2 = 0.0
+                 LC(name='s_sym', category='imposed-load', file_name='LC2.csv',
+                    exclusive_to=['s_asym', 'WF'],
+                     psi_0=0.5, psi_1=0.2, psi_2=0.0
                      ),
                  # LC3:
-                 LC(name = 's_asym', category = 'imposed-load', file_name = 'LC3.csv',
-                    exclusive_to = ['s_sym', 'WF'],
-                     psi_0 = 0.5, psi_1 = 0.2, psi_2 = 0.0
+                 LC(name='s_asym', category='imposed-load', file_name='LC3.csv',
+                    exclusive_to=['s_sym', 'WF'],
+                     psi_0=0.5, psi_1=0.2, psi_2=0.0
                      ),
                  # LC4:
-                 LC(name = 'w_neg', category = 'imposed-load', file_name = 'LC4.csv',
-                     exclusive_to = ['w_pos', 'w_asym', 'w_int', 'WF'],
-                     psi_0 = 0.6, psi_1 = 0.2, psi_2 = 0.0
+                 LC(name='w_neg', category='imposed-load', file_name='LC4.csv',
+                     exclusive_to=['w_pos', 'w_asym', 'w_int', 'WF'],
+                     psi_0=0.6, psi_1=0.2, psi_2=0.0
                      ),
                  # LC5:
-                 LC(name = 'w_pos', category = 'imposed-load', file_name = 'LC5.csv',
-                     exclusive_to = ['w_neg', 'w_asym', 'w_int', 'WF'],
-                     psi_0 = 0.6, psi_1 = 0.2, psi_2 = 0.0
+                 LC(name='w_pos', category='imposed-load', file_name='LC5.csv',
+                     exclusive_to=['w_neg', 'w_asym', 'w_int', 'WF'],
+                     psi_0=0.6, psi_1=0.2, psi_2=0.0
                      ),
                  # LC6:
                  # w_asym:
                  #
-                 LC(name = 'w_asym', category = 'imposed-load', file_name = 'LC6.csv',
-                     exclusive_to = ['w_pos', 'w_neg', 'w_int', 'WF'],
-                     psi_0 = 0.6, psi_1 = 0.2, psi_2 = 0.0
+                 LC(name='w_asym', category='imposed-load', file_name='LC6.csv',
+                     exclusive_to=['w_pos', 'w_neg', 'w_int', 'WF'],
+                     psi_0=0.6, psi_1=0.2, psi_2=0.0
                      ),
                  # LC7:
-                 LC(name = 'w_int', category = 'imposed-load', file_name = 'LC7.csv',
-                     exclusive_to = ['w_pos', 'w_neg', 'w_asym', 'WF', 'T_shrinkage'],
-                     psi_0 = 0.6, psi_1 = 0.2, psi_2 = 0.0,
-                     comment = 'Bauzustand'
+                 LC(name='w_int', category='imposed-load', file_name='LC7.csv',
+                     exclusive_to=['w_pos', 'w_neg', 'w_asym', 'WF', 'T_shrinkage'],
+                     psi_0=0.6, psi_1=0.2, psi_2=0.0,
+                     comment='Bauzustand'
                      ),
-                 # LC8: 
-                 LC(name = 'Q_corner', category = 'imposed-load', file_name = 'LC8.csv',
-                    exclusive_to = ['Q_edge', 'WF'], psi_0 = 0.0, psi_1 = 0.0, psi_2 = 0.0,
-                    comment = '1 kN man load (corner)'
+                 # LC8:
+                 LC(name='Q_corner', category='imposed-load', file_name='LC8.csv',
+                    exclusive_to=['Q_edge', 'WF'], psi_0=0.0, psi_1=0.0, psi_2=0.0,
+                    comment='1 kN man load (corner)'
                     ),
-                 # LC9: 
-                 LC(name = 'Q_edge', category = 'imposed-load', file_name = 'LC9.csv',
-                     exclusive_to = ['Q_corner', 'WF'], psi_0 = 0.0, psi_1 = 0.0, psi_2 = 0.0,
-                     comment = '1 kN man load (edge, center)'
+                 # LC9:
+                 LC(name='Q_edge', category='imposed-load', file_name='LC9.csv',
+                     exclusive_to=['Q_corner', 'WF'], psi_0=0.0, psi_1=0.0, psi_2=0.0,
+                     comment='1 kN man load (edge, center)'
                      ),
                  # LC10:
-                 LC(name = 'T_pos', category = 'imposed-load', file_name = 'LC10.csv',
-                     exclusive_to = ['T_neg', 'WF'], psi_0 = 0.6, psi_1 = 0.5, psi_2 = 0.0,
-                     comment = 'temperature (sommer)'
+                 LC(name='T_pos', category='imposed-load', file_name='LC10.csv',
+                     exclusive_to=['T_neg', 'WF'], psi_0=0.6, psi_1=0.5, psi_2=0.0,
+                     comment='temperature (sommer)'
                      ),
                  # LC11:
-                 LC(name = 'T_neg', category = 'imposed-load', file_name = 'LC11.csv',
-                     exclusive_to = ['T_pos', 'WF'], psi_0 = 0.6, psi_1 = 0.5, psi_2 = 0.0,
-                     comment = 'temperature (winter)'
+                 LC(name='T_neg', category='imposed-load', file_name='LC11.csv',
+                     exclusive_to=['T_pos', 'WF'], psi_0=0.6, psi_1=0.5, psi_2=0.0,
+                     comment='temperature (winter)'
                      ),
                  # LC12:
-                 LC(name = 'T_shrinkage', category = 'imposed-load', file_name = 'LC12.csv',
-                     exclusive_to = ['WF'], psi_0 = 0.8, psi_1 = 0.7, psi_2 = 0.5,
-                     comment = 'shrinkage, combination coefficients taken from case "general imposed load"'
+                 LC(name='T_shrinkage', category='imposed-load', file_name='LC12.csv',
+                     exclusive_to=['WF'], psi_0=0.8, psi_1=0.7, psi_2=0.5,
+                     comment='shrinkage, combination coefficients taken from case "general imposed load"'
                      ),
                ]
 
@@ -194,25 +194,73 @@ if __name__ == '__main__':
 #               ]
 
 #--------------------------------------------------------
+# evaluation for number of reinforcement layers
+#--------------------------------------------------------
+#    lct = LCCTableULS(data_dir = data_dir,
+#                      data_filter = remove_midpoints,
+#                      lc_list = lc_list,
+#                      show_lc_characteristic = False
+#                      )
+#
+# #    lct.configure_traits()
+#    lct.plot_assess_value()
+# #    lct.plot_n_tex()
 
 
-    lct = LCCTableULS(data_dir = data_dir,
-                      data_filter = remove_midpoints,
-                      lc_list = lc_list,
-                      show_lc_characteristic = False
+#--------------------------------------------------------
+# evaluation for eta_n-eta_m-interaction with varying angle of deflection
+#--------------------------------------------------------
+
+
+    # NOTE: resistance values in 'ls_table' file need to be set to sfb-demonstrator values!
+    lct = LCCTableULS(data_dir=data_dir,
+                      data_filter=remove_midpoints,
+                      lc_list=lc_list,
+                      show_lc_characteristic=False,
+                      k_alpha_min=False,  # NO simplification used for 'k_alpha' on the resistance side
                       )
 
-#    lct.configure_traits()
-    lct.plot_assess_value()
-#    lct.plot_n_tex()
+    #--------------------------------------------------------------
+    # 'combi_arr': array with indices of all loading case combinations
+    #--------------------------------------------------------------
+    #
+    print 'lct.combi_arr', lct.combi_arr.shape
+    np.savetxt('combi_arr_LC1-12', lct.combi_arr, delimiter=';')
 
+    #--------------------------------------------------------------
+    # nm-interaction plot (normal force - bending moment)
+    #--------------------------------------------------------------
+    #
+    lct.plot_nm_interaction(save_fig_to_file='nm_interaction_LC1-12')
+
+    #--------------------------------------------------------------
+    # interaction plot of material usage 'eta_nm' (utilization ratio)
+    #--------------------------------------------------------------
+    #
+    lct.plot_eta_nm_interaction(save_fig_to_file='eta_nm_interaction_LC1-12')
+
+    #--------------------------------------------------------------
+    # plot of structure with color indication of material usage 'eta_nm' (Ausnutzungsgrad)
+    # (surrounding values of all loading cases)
+    #--------------------------------------------------------------
+    #
+    lct.plot_assess_value()
+
+    #--------------------------------------------------------------
+    # brows the loading case combinations within an interactive table view
+    #--------------------------------------------------------------
+#    lct.configure_traits()
+
+
+
+#--------------------------------------------------------------
 #    lct = LCCTableSLS( data_dir = data_dir,
 #                      data_filter = remove_midpoints,
 #                       lc_list = lc_list,
 #                       combination_SLS = 'rare',
-##                       combination_SLS = 'freq',
-##                       combination_SLS = 'perm',
-##                       show_lc_characteristic = True
+# #                       combination_SLS = 'freq',
+# #                       combination_SLS = 'perm',
+# #                       show_lc_characteristic = True
 #                        )
 
 
