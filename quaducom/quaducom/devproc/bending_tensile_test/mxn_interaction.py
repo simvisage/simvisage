91a6b447b19762dda0b8bdefe5bcb5e70f19fd39
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2014-07-01 15:31:39 +0200

    Script for the specification aramis_cdt and matresdev interaction.

--- quaducom/quaducom/devproc/bending_tensile_test/mxn_interaction.py
+++ quaducom/quaducom/devproc/bending_tensile_test/mxn_interaction.py
@@ -17,8 +17,6 @@ if __name__ == '__main__':
                            '2014-06-12_BTT-6c-2cm-0-TU_MxN2',
                            'BTT-6c-2cm-TU-0-V14_MxN2.DAT')
 
-
-
     e1 = ExpBTTDB(data_file=test_file)
     e1.process_source_data()
     print 'F_max1', e1.F_max1
