b2781b97f70a3b281cd440e37d8efc1af3e0b75c
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2014-07-01 15:02:23 +0200

    basic script for testing the btt_db class

--- quaducom/quaducom/devproc/bending_tensile_test/exp_btt_db.py
+++ quaducom/quaducom/devproc/bending_tensile_test/exp_btt_db.py
@@ -504,7 +504,18 @@ if __name__ == '__main__':
 #        print inst.production_date
 #        inst.save()
 
-    ExpBTTDB.db.configure_traits()
+    from matresdev.db.simdb import SimDB
+    simdb = SimDB()
+    import os
 
+    test_file = os.path.join(simdb.exdata_dir,
+                           'bending_tensile_test',
+                           '2014-06-12_BTT-6c-2cm-0-TU_MxN2',
+                           'BTT-6c-2cm-TU-0-V14_MxN2.DAT')
 
+    e1 = ExpBTTDB(data_file=test_file)
+    e1.process_source_data()
+    print 'F_max1', e1.F_max1
+
+    # ExpBTTDB.db.configure_traits()
 
---
+++ quaducom/quaducom/devproc/bending_tensile_test/mxn_interaction.py
@@ -0,0 +1,24 @@
+'''
+Created on Jul 1, 2014
+
+Script evaluating the mxn interaction between
+tensile and bending load
+'''
+
+if __name__ == '__main__':
+
+    from exp_btt_db import ExpBTTDB
+    from matresdev.db.simdb import SimDB
+    simdb = SimDB()
+    import os
+
+    test_file = os.path.join(simdb.exdata_dir,
+                           'bending_tensile_test',
+                           '2014-06-12_BTT-6c-2cm-0-TU_MxN2',
+                           'BTT-6c-2cm-TU-0-V14_MxN2.DAT')
+
+
+
+    e1 = ExpBTTDB(data_file=test_file)
+    e1.process_source_data()
+    print 'F_max1', e1.F_max1
