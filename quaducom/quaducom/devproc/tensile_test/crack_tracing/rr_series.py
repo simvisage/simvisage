5b8321986f537c31920bc848fb1e8f36504c7a29
Václav Sadílek <kelidas@centrum.cz>
2015-01-29 11:09:34 +0100

    minor changes - interpolating over time; tests with plotting of Aramis data over data of testing machine

--- quaducom/quaducom/devproc/tensile_test/crack_tracing/rr_series.py
+++ quaducom/quaducom/devproc/tensile_test/crack_tracing/rr_series.py
@@ -3,21 +3,39 @@ Created on Jan 28, 2015
 
 '''
 
-from exp_att_db import ExpATTDB
+from exp_att_db import ExpATTDB, f_interp1d
 from matresdev.db.simdb import SimDB
 simdb = SimDB()
 import os
 import numpy as np
 import pylab as p
+params = {'legend.fontsize': 10,
+          #'legend.linewidth': 2
+          }
+p.rcParams.update(params)
 
-test_files = ['TTb-4c-2cm-0-TU-V%d.DAT' % i for i in range(1, 6)]
-
+test_files = ['TTb-4c-2cm-0-TU-V%d.DAT' % i for i in range(1, 3)]
 test_file_path = os.path.join(simdb.exdata_dir,
                               'tensile_tests', 'buttstrap_clamping',
                               '2013-12-01_TTb-4c-2cm-0-TU_Aramis2d_RR'
                               )
 
+# test_files = ['TTb-6c-2cm-0-TU-V%d.DAT' % i for i in range(1, 2)]
+# test_file_path = os.path.join(simdb.exdata_dir,
+#                               'tensile_tests', 'buttstrap_clamping',
+#                               '2013-12-02_TTb-6c-2cm-0-TU_Aramis2d_RR'
+#                               )
+#
 res_key = 'Xf15s1-Yf15s4'
+
+# test_files = ['TTb-4c-2cm-0-TU-V%d_bs4.DAT' % i for i in range(1, 2)]
+#
+# test_file_path = os.path.join(simdb.exdata_dir,
+#                               'tensile_tests', 'buttstrap_clamping',
+#                               '2013-07-09_TTb-4c-2cm-0-TU_bs4-Aramis3d'
+#                               )
+#
+# res_key = 'Xf19s1-Yf19s4'
 
 e_list = [ExpATTDB(data_file=os.path.join(test_file_path, test_file),
                    aramis_resolution_key=res_key,
@@ -43,14 +61,22 @@ def plot_all():
 
         axes.plot(e.t_aramis_cut, e.F_t_aramis, color='darkblue', label='F')
         axes.plot(e.time_asc, e.F_asc, color='gray', label='F2')
+
+        x = e.aramis_cdt.aramis_data.step_times + e.aramis_start_time
+        stress = e.aramis_cdt.aramis_data.ad_channels_arr[:, 1]
+        axes.plot(x, stress, color='red')
         axes.grid()
-        axes.set_ylim(0, 55)
+        #axes.set_ylim(0, 55)
         axes.set_xlabel('t [sec]')
         axes.set_ylabel('F [kN]')
         axes.legend(loc=2)
 
         axes = p.subplot(232)
 
+        e.aramis_cdt.run_t = True
+        idx = np.argwhere(e.aramis_cdt.force == np.max(e.aramis_cdt.force))
+        axes.plot(e.aramis_cdt.aramis_data.step_times[:idx + 1],
+                  e.aramis_cdt.control_strain_t[:idx + 1], color='lightblue', label='aramis eps')
         axes.plot(e.time_asc, -e.eps_asc, color='darkblue', label='eps')
         axes.grid()
         axes.set_xlabel('t [sec]')
@@ -59,6 +85,15 @@ def plot_all():
         axes.set_title(test_files)
 
         axes = p.subplot(233)
+        axes.plot(-e.eps_asc, e.F_asc, color='darkblue', label='eps')
+        axes.plot(e.aramis_cdt.control_strain_t[:idx + 1], e.aramis_cdt.force[:idx + 1],
+                  color='lightblue', label='aramis eps')
+        axes.plot(e.aramis_cdt.control_strain_t[:idx + 1], f_interp1d(e.aramis_cdt.aramis_data.step_times[:idx + 1] + e.aramis_start_time, e.time_asc, e.F_asc),
+                  color='red', label='aramis eps')
+        axes.grid()
+        axes.set_xlabel('eps [-]')
+        axes.set_ylabel('F [kN]')
+        axes.legend(loc=2)
 
         print '-----------------------------n_steps', e.aramis_info.number_of_steps
         print '-----------------------------n_steps_cut1', len(e.t_aramis)
@@ -71,6 +106,5 @@ def plot_all():
 
 
 if __name__ == '__main__':
-    fig = p.figure()
     plot_all()
     p.show()
