25c4f57b3e44528a3452ead7c6f9ad598630f185
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2015-02-26 14:39:57 +0100

    minor change

--- quaducom/quaducom/devproc/tensile_test/crack_tracing/rr_series.py
+++ quaducom/quaducom/devproc/tensile_test/crack_tracing/rr_series.py
@@ -17,7 +17,7 @@ params = {'legend.fontsize': 10,
           }
 p.rcParams.update(params)
 
-test_files = ['TTb-6c-2cm-0-TU-V%d.DAT' % i for i in [2]]
+test_files = ['TTb-6c-2cm-0-TU-V%d.DAT' % i for i in [5]]
 test_file_path = os.path.join(simdb.exdata_dir,
                               'tensile_tests', 'buttstrap_clamping',
                               '2013-12-02_TTb-6c-2cm-0-TU_Aramis2d_RR'
@@ -48,7 +48,7 @@ param_dict = {'TTb-4c-2cm-0-TU-V1.DAT': (22, -4e-4, 0.003),
               'TTb-6c-2cm-0-TU-V5.DAT': (20, -2.5e-4, 0.0015)
               }
 
-e_list = e_list_4c
+e_list = e_list_6c
 
 
 def plot_all():
