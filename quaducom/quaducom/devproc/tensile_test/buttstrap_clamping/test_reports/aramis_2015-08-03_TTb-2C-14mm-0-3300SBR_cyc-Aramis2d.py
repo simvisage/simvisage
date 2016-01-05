f185e81ab6f9f01160bf8a55c9ef5493668b937b
ale-x <ascholzen@imb.rwth-aachen.de>
2016-01-05 08:52:00 +0100

    test reports scripts for aramis evaluation of tensile tests;

--- quaducom/quaducom/devproc/tensile_test/buttstrap_clamping/test_reports/aramis_2015-08-03_TTb-2C-14mm-0-3300SBR_cyc-Aramis2d.py
+++ quaducom/quaducom/devproc/tensile_test/buttstrap_clamping/test_reports/aramis_2015-08-03_TTb-2C-14mm-0-3300SBR_cyc-Aramis2d.py
@@ -1,13 +1,12 @@
 '''
-Created on Jul 1, 2014
-
-Script evaluating the mxn interaction between
-tensile and bending load
+Script evaluating the aramis data
+for tensile tests
+Created on Jan 4, 2015 by scho
 '''
 
 if __name__ == '__main__':
 
-    from exp_btt_db import ExpBTTDB
+    from exp_tt_aramis2d import ExpTTAramis2d
     from matresdev.db.simdb import SimDB
     from aramis_cdt import AramisUI
     simdb = SimDB()
@@ -22,62 +21,142 @@ if __name__ == '__main__':
     #------------------------
     # Aramis2d - frontview
     #------------------------
-     test_files = ['TTb-2C-14mm-0-3300SBR-V1_cyc-Aramis2d.DAT']
-
-#     res_key = 'Xf15s13-Yf15s13'
+    test_file = 'TTb-2C-14mm-0-3300SBR-V1_cyc-Aramis2d.DAT'
+    scale_data_factor = 1.385  # based on real specimen width of the tensile tests with SBR textile (= 96 mm)
     res_key = 'Xf15s1-Yf15s4'
+    start_time_aramis = 6.
 
-    e_list = [ExpBTTDB(data_file=os.path.join(test_file_path, test_file),
+#     scale_data_factor = 1.40  # based on real specimen width of the tensile tests with SBR textile (= 96 mm)
+#     res_key = 'Xf15s13-Yf15s13'
+
+    #-------------------------------------------
+    # specify parameters for aramis_cdt
+    #-------------------------------------------
+    crack_detaction_step = -1  # last step (not used if 'current_step'=True)
+    crack_detaction_current_step = True
+    integ_radius = 3  # radius used for crack detection
+    integ_radius_crack = 5  # radius used for calculation of displacement jumps (=crack width)
+    dd_ux_avg_threshold = -1e-07
+    dd_ux_threshold = -1e-07
+    ddd_ux_avg_threshold = 0.00001
+    ddd_ux_threshold = 0.00001
+
+    #-------------------------------------------
+    # setup experiment run with aramis data
+    #-------------------------------------------
+    e_run = ExpTTAramis2d(data_file=os.path.join(test_file_path, test_file),
                        aramis_resolution_key=res_key,
-                       delta_t_aramis=5)
-              for test_file in test_files]
-    for e in e_list:
-        e.process_source_data()
-        print 'crack filter', e.crack_filter_avg
-        print e.n_steps
-        e.aramis_field_data.integ_radius = 3
-#         e.aramis_field_data.current_step = e.n_steps
-#         e.aramis_cdt.crack_detection_step = e.n_steps
-        e.aramis_cdt.ddd_ux_avg_threshold = -0.5e-3
-        e.aramis_cdt.ddd_ux_threshold = -0.5e-3
+                       start_time_aramis=start_time_aramis,
+                       integ_radius=integ_radius,
+                       integ_radius_crack=integ_radius_crack,
+                       ddd_ux_avg_threshold=ddd_ux_avg_threshold,
+                       ddd_ux_threshold=ddd_ux_threshold,
+                       dd_ux_avg_threshold=dd_ux_avg_threshold,
+                       dd_ux_threshold=dd_ux_threshold,
+                       delta_t_aramis=0,
+                       crack_detaction_current_step=crack_detaction_current_step,
+                       crack_detaction_step=crack_detaction_step
+                       )
 
-        x = e.aramis_field_data.x_arr_0
-        y = e.aramis_field_data.y_arr_0
-        print 'Measuring field length (bottom, top) =', x[0, -1] - x[0, 0], x[-1, -1] - x[-1, 0]
-        print 'Measuring field height (left, right) =', y[0, 0] - y[-1, 0], y[0, -1] - y[-1, -1]
-        xs = 3  # step in x-direction
-        dist = x[:, 1:] - x[:, :-1]
-        print 'Mean, std, min, max of facets center distance [mm]: ', np.mean(dist), np.std(dist), np.min(dist), np.max(dist)
-        print 'Mean and std of pixel size [mm]', np.mean(dist / xs), np.std(dist / xs)
+    e_run.process_source_data()
+    print 'crack filter', e_run.crack_filter_avg
 
+    #-------------------------------------------
+    # non-deformed coordinates of the measuring field
+    #-------------------------------------------
+    x = e_run.aramis_field_data.x_arr_0
+    y = e_run.aramis_field_data.y_arr_0
+    z = e_run.aramis_field_data.z_arr_0
 
-        range_l = max(x.shape[0], x.shape[1])
-        print 'range_l', range_l
-        range_t = min(x.shape[0], x.shape[1])
-        print 'range_t', range_t
-        dist_l = x[0, -1] - x[0, 0]
-        print 'dist_l', dist_l
-        dist_t = y[0, 0] - y[-1, 0]
-        print 'dist_t', dist_t
+    #-------------------------------------------
+    # dimensions of the measuring field (as read in from file)
+    #-------------------------------------------
+    lx_bottom = x[0, -1] - x[0, 0]
+    lx_top = x[-1, -1] - x[-1, 0]
+    ly_left = y[0, 0] - y[-1, 0]
+    ly_right = y[0, -1] - y[-1, -1]
+    lx_avg = (lx_bottom + lx_top) / 2.
+    ly_avg = (ly_right + ly_left) / 2.
+    print 'Measuring field length: lx_bottom ', lx_bottom
+    print 'Measuring field length: lx_top ', lx_top
+    print 'Measuring field length: ly_left ', ly_left
+    print 'Measuring field length: ly_right ', ly_right
+    print 'Measuring field length: lx_avg ', lx_avg
+    print 'Measuring field length: ly_avg ', ly_avg
 
-        overlap_factor = 1.05
-        glyph_l_length = dist_l / range_l
-        glyph_l_length *= overlap_factor
-        print 'glyph_l_length', glyph_l_length
-        glyph_t_length = dist_t / range_t
-        glyph_t_length *= overlap_factor
-        print 'glyph_t_length', glyph_t_length
+    #-------------------------------------------
+    # resolution used for aramis evaluation
+    #-------------------------------------------
+    res_key_list = res_key.split('-')
+    X_list = res_key_list[0].split('Xf')[1].split('s')
+    Y_list = res_key_list[1].split('Yf')[1].split('s')
+    Xf = int(X_list[0])
+    Xs = int(X_list[1])
+    Yf = int(Y_list[0])
+    Ys = int(Y_list[1])
+    print 'res_key_list', res_key_list
+    print 'X_list', X_list
+    print 'Y_list', Y_list
 
-        scale_factor_cr = 10.
+    #-------------------------------------------
+    # average pixel size
+    #-------------------------------------------
+    dist_x = x[:, 1:] - x[:, :-1]
+    lenght_px_x = np.mean(dist_x / Xs)
+    dist_y = y[:, 1:] - y[:, :-1]
+    lenght_px_y = np.mean(dist_y / Ys)
+    print 'Mean, std, min, max of facets center distance [mm]: x-direction', np.mean(dist_x), np.std(dist_x), np.min(dist_x), np.max(dist_x)
+    print 'Mean, std, min, max of facets center distance [mm]: y-direction', np.mean(dist_y), np.std(dist_y), np.min(dist_y), np.max(dist_y)
+    print 'Mean and std of pixel size [mm]: x-direction', np.mean(dist_x / Xs), np.std(dist_x / Xs)
+    print 'Mean and std of pixel size [mm]: y-direction', np.mean(dist_y / Ys), np.std(dist_y / Ys)
 
-        aui = AramisUI(aramis_info=e.aramis_info,
-                       aramis_data=e.aramis_field_data,
-                       aramis_cdt=e.aramis_cdt,
-                       glyph_x_length=glyph_l_length,
-                       glyph_y_length=glyph_t_length,
-                       glyph_z_length=0.00,
-                       glyph_x_length_cr=60.0 * glyph_l_length,
-                       glyph_y_length_cr=scale_factor_cr * glyph_l_length,
-                       glyph_z_length_cr=scale_factor_cr * glyph_t_length,
-                       warp_factor=1.0)
-        aui.configure_traits()
+    #-------------------------------------------
+    # apply scale factor 2d to coordinates, displacements and crack field arrays
+    #-------------------------------------------
+    e_run.aramis_field_data.scale_data_factor = scale_data_factor
+
+    #-------------------------------------------
+    # cut off the boundary cells that can not be evaluated as they exceed the integ_radius-zone
+    #-------------------------------------------
+    left_i = e_run.aramis_field_data.left_i
+    e_run.aramis_field_data.left_i = integ_radius_crack
+    right_i = e_run.aramis_field_data.right_i
+    e_run.aramis_field_data.right_i -= integ_radius_crack
+    print 'left_i', left_i
+    print 'right_i', right_i
+
+    #-------------------------------------------
+    # scale gylphs for mlab.points3d
+    #-------------------------------------------
+    overlap_factor = 1.03  # cubes overlap for smoother display without gaps
+    range_l = max(x.shape[0], x.shape[1])
+    range_t = min(x.shape[0], x.shape[1])
+    dist_l = lx_avg * scale_data_factor
+    dist_t = ly_avg * scale_data_factor
+    glyph_l_length = dist_l / range_l
+    glyph_l_length *= overlap_factor
+    glyph_t_length = dist_t / range_t
+    glyph_t_length *= overlap_factor
+    print 'range_l', range_l
+    print 'range_t', range_t
+    print 'dist_l', dist_l
+    print 'dist_t', dist_t
+    print 'glyph_l_length', glyph_l_length
+    print 'glyph_t_length', glyph_t_length
+
+    #-------------------------------------------
+    # start aramis user interface
+    #-------------------------------------------
+    aui = AramisUI(aramis_info=e_run.aramis_info,
+                   aramis_data=e_run.aramis_field_data,
+                   aramis_cdt=e_run.aramis_cdt,
+                   glyph_x_length=glyph_l_length,
+                   glyph_y_length=glyph_t_length,
+                   glyph_z_length=0.0,
+                   glyph_x_length_cr=glyph_l_length * 6.,  # extrude the plot_var in the out-of-plane direction
+                   glyph_y_length_cr=glyph_l_length,
+                   glyph_z_length_cr=glyph_t_length,
+                   warp_factor=1.0)
+
+    aui.configure_traits()
+
