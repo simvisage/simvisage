5a67fa3780a8c58828cecb8c4d22692a83974d72
jbielak <jbielak@imb.rwth-aachen.de>
2016-04-15 10:57:06 +0200

    Minor Changes by Rostia

--- quaducom/quaducom/devproc/double_pullout/test_reports/report_2016-03-16x_DPO-3300SBR_R4_compare_AC_DD.py
+++ quaducom/quaducom/devproc/double_pullout/test_reports/report_2016-03-16x_DPO-3300SBR_R4_compare_AC_DD.py
@@ -19,7 +19,7 @@ print scipy.__version__
 
 class PlotBase(HasTraits):
 
-    def figure(self, dataset):
+    def figure(self):
         fig = p.figure(facecolor='white', figsize=(12, 9))
         fig.subplots_adjust(
             left=0.07, right=0.97, bottom=0.08, top=0.96, wspace=0.25, hspace=0.2)
@@ -31,19 +31,21 @@ class PlotRotation(PlotBase):
 
     def plot(self, dataset, axes, linestyle=None, color=None, linewidth=1.5):
         gauge_dist = dataset.gauge_dist
-        for idx, (e_run, n_r) in enumerate(zip(dataset.e_list, dataset.n_roving_list)):
-            e = e_run.ex_type
-            max_F_idx = np.argmax(e.Kraft)
-            if linestyle == None:
-                linestyle = dataset.linestyle_list[idx]
-            if color == None:
-                color = dataset.color_list[idx]
-            w_left = getattr(e, dataset.left_gauge_name)
-            w_right = getattr(e, dataset.right_gauge_name)
-            axes.plot(e.Kraft[:max_F_idx] / n_r,
-                      (np.fabs(w_right - w_left)[:max_F_idx]) / gauge_dist,
-                      linewidth=linewidth,
-                      linestyle=linestyle, color=color)
+        for idx, (e_treatment, e_treatment_n_r) in enumerate(zip(dataset.e_array,
+                                                                 dataset.n_roving_array)):
+            for e_run, n_r in zip(e_treatment, e_treatment_n_r):
+                e = e_run.ex_type
+                max_F_idx = np.argmax(e.Kraft)
+                if linestyle == None:
+                    linestyle = dataset.linestyle_list[idx]
+                if color == None:
+                    color = dataset.color_list[idx]
+                w_left = getattr(e, dataset.left_gauge_name)
+                w_right = getattr(e, dataset.right_gauge_name)
+                axes.plot(e.Kraft[:max_F_idx] / n_r,
+                          (np.fabs(w_right - w_left)[:max_F_idx]) / gauge_dist,
+                          linewidth=linewidth,
+                          linestyle=linestyle, color=color)
 
     def decorate(self, axes):
         axes.grid()
@@ -51,7 +53,6 @@ class PlotRotation(PlotBase):
         axes.set_xlabel('force per roving [kN]')
         axes.legend(loc=2)
         axes.axis([0., 2.5, 0., 0.025])
-ac
 
 
 class PlotFW(PlotBase):
@@ -110,8 +111,8 @@ class PlotFWAvg(PlotBase):
 
 if __name__ == '__main__':
 
-    pw = PlotFW()
-    ax = pw.figure(dd)
+    pw = PlotRotation()
+    ax = pw.figure()
     pw.plot(dd, ax, color='black')
     pw.plot(dd_po, ax, color='blue')
     pw.plot(ac, ax, linestyle='dashed', color='red', linewidth=2)
